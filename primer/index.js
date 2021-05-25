330c7b7942a48130e02f8f29aa5b0255dd3cb17f
OmarMWarraich <earthling986@gmail.com>
2021-05-25 12:04:55 -0400

    Unintentional effect of Applied TypeCoercion- logical OR op uses fallback value when taxRate var is 0 or undefined(coerced 0)

--- primer/index.js
+++ primer/index.js
@@ -3,20 +3,7 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
-if (hatPrice === bootsPrice) {
-    console.log("Prices are the same.");
-} else {
-    console.log("Prices are different.");
-}
-
-let totalPrice = hatPrice + bootsPrice;
-console.log(`Total Price: ${totalPrice}`);
-
-let myVariable = "Adam";
-console.log(`Type: ${typeof myVariable}`);
-myVariable = 100;
-console.log(`Type: ${typeof myVariable}`);
-
-let firstCity;
-let secondCity = firstCity || "London";
-console.log(`City: ${ secondCity }`);
+let taxRate; // no tax rate has been defined
+console.log(`Tax rate: ${taxRate || 10}%`);
+taxRate = 0; // zero-rated for tax
+console.log(`Tax rate: ${taxRate || 10}%`);
