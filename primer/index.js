01503cc34cc5f6b0f3c80f7f1613c09876df3422
OmarMWarraich <earthling986@gmail.com>
2021-05-25 11:51:32 -0400

    Avoid Unintentional Type Coercion nd Apply Explicit Type Coercion

--- primer/index.js
+++ primer/index.js
@@ -3,7 +3,7 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
-if (hatPrice == bootsPrice) {
+if (hatPrice === bootsPrice) {
     console.log("Prices are the same.");
 } else {
     console.log("Prices are different.");
@@ -16,3 +16,7 @@ let myVariable = "Adam";
 console.log(`Type: ${typeof myVariable}`);
 myVariable = 100;
 console.log(`Type: ${typeof myVariable}`);
+
+let firstCity;
+let secondCity = firstCity || "London";
+console.log(`City: ${ secondCity }`);
