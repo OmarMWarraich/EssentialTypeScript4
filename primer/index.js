5271192190946b4494e9066c0f8a53402e9d6df3
OmarMWarraich <earthling986@gmail.com>
2021-05-25 14:53:33 -0400

    Invoke a function as a method

--- primer/index.js
+++ primer/index.js
@@ -1,16 +1,17 @@
-function writeMessage(message) {
-    console.log(`${this.greeting}, ${message}`);
-}
+let myObject = {
+    greeting: "Hi, there",
+
+    writeMessage(message) {
+        console.log(`${this.greeting}, ${message}`)
+    }
+};
 
 greeting = "Hello";
 
-writeMessage("It is sunny today");
-writeMessage.call(global, "It is sunny today");
+myObject.writeMessage("It is sunny today");
 
-// The writeMessage fn reads a prop named greeting from this.
-//Value assigned names  without using the let, const, or var keyword are assigned to
-// the global object. The statement that assigns the string value Hello creates a variable
-// in the global scope. call fn, this is assigned the global object.
+// When the function is invoked via the object, the statement that invokes the function 
+// is akin to using the call method with the object as the first argument.
 
-// The new statement uses the call method directly and sets the this value to the global
-// object, with the same result as the conventional function call before it.
+// Care is required because this is set differently if the function is accessed out of
+// its object, which can happen if the function is assigned to a variable.
