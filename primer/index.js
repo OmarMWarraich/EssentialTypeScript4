5f48a1ddb520792a9a9493a34e7f716c5c851938
OmarMWarraich <earthling986@gmail.com>
2021-05-25 12:34:32 -0400

    Use Rest Parameter

--- primer/index.js
+++ primer/index.js
@@ -3,8 +3,10 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
-function sumPrices(first, second, third = 0) {
-    return first + second + third;
+function sumPrices(...numbers) {
+    return numbers.reduce(function(total, val) {
+        return total + val
+    }, 0);
 }
 
 let totalPrice = sumPrices(hatPrice,bootsPrice);
