1604d6b4d0b92373bc3b00b1f6263d18aa65251d
OmarMWarraich <earthling986@gmail.com>
2021-05-25 12:49:20 -0400

    Replace function in src/index.js

--- primer/index.js
+++ primer/index.js
@@ -3,10 +3,8 @@ console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
-function sumPrices(...numbers) {
-    return numbers.reduce((total, val) => 
+let sumPrices = (...numbers) => numbers.reduce((total, val) => 
         total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
-    }
 
 let totalPrice = sumPrices(hatPrice,bootsPrice);
 console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
