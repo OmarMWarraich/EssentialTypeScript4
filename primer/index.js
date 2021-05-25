1d82b226e4ee74440581a748abfc711468b229c4
OmarMWarraich <earthling986@gmail.com>
2021-05-25 14:09:49 -0400

    Add, Replace and Absorb Properties

--- primer/index.js
+++ primer/index.js
@@ -8,5 +8,11 @@ let boots = {
     price: "100"
 }
 
-let otherHat = { ...hat };
-console.log(`Spread: ${otherHat.name}, ${otherHat.price}`);
+let additionalProperties = { ...hat, discounted: true };
+console.log(`Additional: ${JSON.stringify(additionalProperties)}`)
+
+let replacedProperties = { ...hat, price: 10};
+console.log(`Replaced: ${JSON.stringify(replacedProperties)}`)
+
+let { price , ...someProperties } = hat;
+console.log(`Selected: ${JSON.stringify(someProperties)}`)
