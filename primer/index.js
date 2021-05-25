a627814147fac2432a0ddf877eaa77fd4b20c4ad
OmarMWarraich <earthling986@gmail.com>
2021-05-25 14:04:04 -0400

    Optional chaining

--- primer/index.js
+++ primer/index.js
@@ -17,8 +17,6 @@ gloves.name = gloves.productName;
 delete gloves.productName;
 gloves.price = 20;
 
-let sumPrices = (...numbers) => numbers.reduce((total, val) =>
-     total +(Number.isNaN(Number(val)) ? 0 : Number(val)));
-
-let totalPrice = sumPrices(hat.price, boots.price, gloves.price);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
+let propertyCheck = hat.price ?? 0;
+let objectAndPropertyCheck = hat ?.price ?? 0;
+console.log(`Checks: ${propertyCheck}, ${objectAndProperyCheck}`)
