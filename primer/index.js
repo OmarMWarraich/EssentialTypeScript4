dae3bd32aca4636e04c18b7ee5f080df08fa73c7
OmarMWarraich <earthling986@gmail.com>
2021-05-25 12:43:25 -0400

    Convert and filter Parameter Values

--- primer/index.js
+++ primer/index.js
@@ -5,7 +5,7 @@ console.log(`Boots price: ${bootsPrice}`);
 
 function sumPrices(...numbers) {
     return numbers.reduce(function(total, val) {
-        return total + val
+        return total + (Number.isNaN(Number(val)) ? 0 : Number(val));
     }, 0);
 }
 
@@ -15,5 +15,5 @@ console.log(`Total Price: ${totalPrice} ${typeof totalPrice}`);
 totalPrice = sumPrices(100, 200, 300);
 console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
 
-totalPrice = sumPrices(100, 200);
+totalPrice = sumPrices(100, 200, undefined, false, "hello");
 console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
