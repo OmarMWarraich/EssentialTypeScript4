3dd6f5d0bb70dff6f92c081c4c941a80058bbce0
OmarMWarraich <earthling986@gmail.com>
2021-05-25 09:37:48 -0400

    Mark Tasks Complete

--- todo/dist/index.js
+++ todo/dist/index.js
@@ -46,7 +46,9 @@ function displayTodoList() {
 var Commands;
 (function (Commands) {
     Commands["Add"] = "Add New Task";
+    Commands["Complete"] = "Complete Task";
     Commands["Toggle"] = "Show/Hide Completed";
+    Commands["Purge"] = "Remove Completed Tasks";
     Commands["Quit"] = "Quit";
 })(Commands || (Commands = {}));
 function promptAdd() {
@@ -56,6 +58,17 @@ function promptAdd() {
         if (answers["add"] !== "") {
             collection.addTodo(answers["add"]);
         }
+        promptUser();
+    });
+}
+function promptComplete() {
+    console.clear();
+    inquirer.prompt({ type: "checkbox", name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map(item => ({ name: item.task, value: item.id, checked: item.complete }))
+    }).then(answers => {
+        let completedTasks = answers["complete"];
+        collection.getTodoItems(true).forEach(item => collection.markComplete(item.id, completedTasks.find(id => id === item.id) != undefined));
         promptUser();
     });
 }
@@ -76,6 +89,18 @@ function promptUser() {
                 break;
             case Commands.Add:
                 promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                }
+                else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
                 break;
         }
     });
--- todo/src/index.ts
+++ todo/src/index.ts
@@ -31,7 +31,9 @@ function displayTodoList(): void {
 
 enum Commands {
     Add = "Add New Task",
+    Complete = "Complete Task",
     Toggle = "Show/Hide Completed",
+    Purge = "Remove Completed Tasks",
     Quit = "Quit"
 }
 
@@ -43,6 +45,21 @@ function promptAdd(): void {
         }
     promptUser();
     })
+}
+
+function promptComplete(): void {
+    console.clear();
+    inquirer.prompt({ type: "checkbox", name: "complete", 
+            message: "Mark Tasks Complete",
+            choices: collection.getTodoItems(showCompleted).map(item =>
+                        ({name: item.task, value: item.id, checked: item.complete}))
+                        }).then(answers => {
+                            let completedTasks = answers["complete"] as number[];
+                            collection.getTodoItems(true).forEach(item =>
+                                collection.markComplete(item.id, 
+                                    completedTasks.find(id => id === item.id) != undefined));
+                                    promptUser();
+                                })                        
 }
 
 function promptUser(): void {
@@ -63,6 +80,18 @@ function promptUser(): void {
             case Commands.Add:
                 promptAdd();
                 break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                } else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
+                break;
+
         }
     })
 }
