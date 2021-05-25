c79172ae4e863f2e584acdfe96fe3e5c2f2ce693
OmarMWarraich <earthling986@gmail.com>
2021-05-25 13:55:54 -0400

    Add, Change and Delete Object Properties

--- primer/index.js
+++ primer/index.js
@@ -8,8 +8,17 @@ let boots = {
     price: "100"
 }
 
+let gloves = {
+    productName: "Gloves",
+    price: "40"
+}
+
+gloves.name = gloves.productName;
+delete gloves.productName;
+gloves.price = 20;
+
 let sumPrices = (...numbers) => numbers.reduce((total, val) =>
      total +(Number.isNaN(Number(val)) ? 0 : Number(val)));
 
-let totalPrice = sumPrices(hat.price, boots.price);
+let totalPrice = sumPrices(hat.price, boots.price, gloves.price);
 console.log(`Total: ${totalPrice} ${typeof totalPrice}`);
