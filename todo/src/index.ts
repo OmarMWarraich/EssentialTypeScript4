840a99c41c5da6ab8f922c218e21435ad8617769
OmarMWarraich <earthling986@gmail.com>
2021-05-25 09:05:57 -0400

    types/inquirer and add commands

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -2,6 +2,7 @@
 
 import { TodoItem } from './todoItem';
 import { TodoCollection } from './todoCollection';
+import * as inquirer from 'inquirer';
 
 let todos: TodoItem[] = [
     new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
@@ -9,18 +10,47 @@ let todos: TodoItem[] = [
 ];
 
 let collection: TodoCollection = new TodoCollection("Adam", todos);
+let showCompleted = true;
 
-console.clear();
-console.log(`${collection.userName}'s Todo List `
+// console.clear();
+
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List `
     + `(${ collection.getItemCounts().incomplete } items to do)`);
+    // let newId: number = collection.addTodo("Go for run");
+    // let todoItem: TodoItem = collection.getTodoById(newId);
+    // // console.log(JSON.stringify(todoItem));
+    // todoItem.printDetails();
 
-// let newId: number = collection.addTodo("Go for run");
-// let todoItem: TodoItem = collection.getTodoById(newId);
-// // console.log(JSON.stringify(todoItem));
-// todoItem.printDetails();
+    //collection.addTodo(todoItem);
+    //Getting the Collection Items.
+    //Testing Item removal 
+    // collection.removeComplete();
+    collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
+}
 
-//collection.addTodo(todoItem);
-//Getting the Collection Items.
-//Testing Item removal 
-// collection.removeComplete();
-collection.getTodoItems(true).forEach(item => item.printDetails());
+enum Commands {
+    Toggle = "Show/Hide Completed",
+    Quit = "Quit"
+}
+
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+            type: "list",
+            name: "command",
+            message: "Choose option",
+            choices: Object.values(Commands),
+            //badProperty: true
+    }).then(answers => {
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+            promptUser();
+                break;
+        }
+    })
+}
+
+promptUser();
