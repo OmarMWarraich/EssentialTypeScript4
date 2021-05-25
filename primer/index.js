d86f265e0854b91013e898e75bd551a168d1f2a7
OmarMWarraich <earthling986@gmail.com>
2021-05-25 13:42:40 -0400

    Destructure an Array

--- primer/index.js
+++ primer/index.js
@@ -1,14 +1,4 @@
 let names = ["Hat", "Boots", "Gloves"];
-let prices = [];
 
-prices.push(100);
-prices.push("100");
-prices.push(50.25);
-
-console.log(`First Item: ${names[0]}: ${prices[0]}`)
-
-let sumPrices = (...numbers) => numbers.reduce((total, val) =>
-    total + (Number.isNaN(Number(val)) ? 0 : Number(val)));
-
-let totalPrice = sumPrices(...prices);
-console.log(`Total: ${totalPrice} ${typeof totalPrice}`)
+let [one, two] = names;
+console.log(`One: ${one}, Two: ${two}`);
