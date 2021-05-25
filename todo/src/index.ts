865214e9851499b1f1ac8dc235055f2b8157a42a
OmarMWarraich <earthling986@gmail.com>
2021-05-25 04:03:46 -0400

    Add Type info in src/index.ts

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -1,2 +1,21 @@
+//Testing the Data Model in the index.ts File in the src Folder
+
+import { TodoItem } from './todoItem';
+import { TodoCollection } from './todoCollection';
+
+let todos: TodoItem[] = [
+    new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)
+];
+
+let collection: TodoCollection = new TodoCollection("Adam", todos);
+
 console.clear();
-console.log("Adam's Todo Listener");
+console.log(`${collection.userName}'s Todo List`);
+
+let newId: number = collection.addTodo("Go for run");
+let todoItem: TodoItem = collection.getTodoById(newId);
+// console.log(JSON.stringify(todoItem));
+todoItem.printDetails();
+
+//collection.addTodo(todoItem);
