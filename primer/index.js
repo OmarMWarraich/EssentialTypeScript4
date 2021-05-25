d8fa4ed3f36200e87b9aa045c110c04e73dd8050
OmarMWarraich <earthling986@gmail.com>
2021-05-25 11:35:01 -0400

    Add Statements in src/index.js

--- primer/index.js
+++ primer/index.js
@@ -1,4 +1,13 @@
-let hatprice = 100;
-console.log(`Hat price: ${hatprice}`);
+let hatPrice = 100;
+console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
+
+if (hatPrice == bootsPrice) {
+    console.log("Prices are the same.");
+} else {
+    console.log("Prices are different.");
+}
+
+let totalPrice = hatPrice + bootsPrice;
+console.log(`Total Price: ${totalPrice}`);
