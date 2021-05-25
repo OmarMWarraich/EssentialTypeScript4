cc67ac4fdf2691f219c2bcc2aa7e930f75551a33
OmarMWarraich <earthling986@gmail.com>
2021-05-25 13:48:09 -0400

    Assign Remaining ELements to an Array

--- primer/index.js
+++ primer/index.js
@@ -2,3 +2,7 @@ let names = ["Hat", "Boots", "Gloves"];
 
 let [,, three] = names;
 console.log(`Three: ${three}`);
+
+let prices = [100, 120, 50.25];
+let [, ...highest] = prices.sort((a,b) => a-b);
+highest.forEach(price => console.log(`High price: ${price}`));
