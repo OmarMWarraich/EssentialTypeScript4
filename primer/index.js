7274d96c37e92b72b3c60f45d75ebbfa6522fbdc
OmarMWarraich <earthling986@gmail.com>
2021-05-25 14:47:49 -0400

    Invoke a function

--- primer/index.js
+++ primer/index.js
@@ -5,8 +5,12 @@ function writeMessage(message) {
 greeting = "Hello";
 
 writeMessage("It is sunny today");
+writeMessage.call(global, "It is sunny today");
 
 // The writeMessage fn reads a prop named greeting from this.
 //Value assigned names  without using the let, const, or var keyword are assigned to
 // the global object. The statement that assigns the string value Hello creates a variable
 // in the global scope. call fn, this is assigned the global object.
+
+// The new statement uses the call method directly and sets the this value to the global
+// object, with the same result as the conventional function call before it.
