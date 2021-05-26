216b087744d0fb21ba232ad6acf1048602e61ba4
OmarMWarraich <earthling986@gmail.com>
2021-05-26 06:43:16 -0400

    Define Multiple Named Features in a Module

--- primer2/index.js
+++ primer2/index.js
@@ -1,4 +1,5 @@
 import calcTaxandSum, {calculateTax} from './tax';
+import { printDetails, applyDiscount } from './utils';
 
 class Product {
     constructor(name, price) {
@@ -9,8 +10,10 @@ class Product {
 }
 
 let product = new Product("Hat", 100);
+applyDiscount(product, 10);
 let taxedPrice = calculateTax(product.price);
-console.log(`Name: ${ product.name }, Taxed Price: ${taxedPrice}`);
+printDetails(product);
+// console.log(`Name: ${ product.name }, Taxed Price: ${taxedPrice}`);
 
 let products = [new Product("Gloves", 23), new Product("Boots", 100)]
 let totalPrice = calcTaxandSum(...products.map(p => p.price));
