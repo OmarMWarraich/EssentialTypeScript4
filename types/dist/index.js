dd261e792ef0d632046d4ee7caf871ba262175ec
OmarMWarraich <earthling986@gmail.com>
2021-05-26 16:34:00 -0400

    The Alternative Type Assertion Syntax

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,23 +1,19 @@
-// Asserting to an Unexpected Type
-// The compiler checks that the type used in an assertion is
-// expected. However, if assertion is to a type that the compiler
-// doesnt expect the following happens. 
+// The Alternative Type Assertion Syntax
 function calculateTax(amount, format) {
     const calcAmount = amount * 1.2;
     return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
 }
 let taxNumber = calculateTax(100, false);
+// Type Assetions can also be performed using an angle bracket
+// syntax so that the following statement
+// let taxString = calculateTax(100, true) as string;
+//becomes
 let taxString = calculateTax(100, true);
+// However this syntax cant be used in TSX files, which combine
+// HTML elements with Typescript code and are commonly used in
+// React dev. Therefore, the as keyword is the preferred way
+// to assert types.
 let taxBoolean = calculateTax(100, false);
 console.log(`Number Value: ${taxNumber.toFixed(2)}`);
 console.log(`String Value: ${taxString.charAt(0)}`);
 console.log(`Boolean Value: ${taxBoolean}`);
-// The assertion can be forced and compiler's warning be 
-// overridden by first asserting to any and then the required
-// type. The compiler error refers to previous commit.
-// The additional step prevents the compiler from warning about
-// the change and treats the result from the function as a 
-// boolean value. However, as noted earlier, assertions only
-// affect the type checking process and do not perform type 
-// coercion, which can be seen in the results produced when the
-// code is compiled.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,8 +1,4 @@
-// Asserting to an Unexpected Type
-
-// The compiler checks that the type used in an assertion is
-// expected. However, if assertion is to a type that the compiler
-// doesnt expect the following happens. 
+// The Alternative Type Assertion Syntax
 
 function calculateTax(amount: number, format: boolean): string | number {
     const calcAmount = amount * 1.2;
@@ -10,20 +6,20 @@ function calculateTax(amount: number, format: boolean): string | number {
 }
 
 let taxNumber = calculateTax(100, false) as number;
-let taxString = calculateTax(100, true) as string;
+
+// Type Assetions can also be performed using an angle bracket
+// syntax so that the following statement
+// let taxString = calculateTax(100, true) as string;
+//becomes
+let taxString = <string> calculateTax(100, true);
+// However this syntax cant be used in TSX files, which combine
+// HTML elements with Typescript code and are commonly used in
+// React dev. Therefore, the as keyword is the preferred way
+// to assert types.
+
 let taxBoolean = calculateTax(100, false) as any as boolean;
 
 console.log(`Number Value: ${taxNumber.toFixed(2)}`);
 console.log(`String Value: ${taxString.charAt(0)}`);
 console.log(`Boolean Value: ${taxBoolean}`);
 
-// The assertion can be forced and compiler's warning be 
-// overridden by first asserting to any and then the required
-// type. The compiler error refers to previous commit.
-
-// The additional step prevents the compiler from warning about
-// the change and treats the result from the function as a 
-// boolean value. However, as noted earlier, assertions only
-// affect the type checking process and do not perform type 
-// coercion, which can be seen in the results produced when the
-// code is compiled.
