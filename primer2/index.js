849aa6e21b20539edd9f7456e9c07894b09ec591
OmarMWarraich <earthling986@gmail.com>
2021-05-26 03:04:36 -0400

    Use an Generator

--- primer2/index.js
+++ primer2/index.js
@@ -1,9 +1,9 @@
-// Using  an Iterator
+// Using  a Generator
 
-// Iterators are objects that return a sequence of values. An iterator defines a function
-// named next that returns an object with value and done properties: the value property
-// returns the next value in the sequence, and the done property is set to true when the
-// sequence is complete.
+// Writing iterators can be awkward because the code has to maintain state data to keep
+// track of the current position in the sequence each time the next function is invoked.
+// A similar approach is to use a generator, which is a function that is invoked once 
+// and uses the yield keyword to produce the values in the sequence.
 
 
 class Product {
@@ -17,31 +17,12 @@ class Product {
     }
 }
 
-function createProductIterator() {
-    const hat = new Product("Hat", 100);
-    const boots = new Product("Boots", 100);
-    const umbrella = new Product("Umbrella", 23);
+// Generator functions are dentoed with an asterik.
 
-    let lastVal;
-
-    return {
-        next() {
-            switch (lastVal) {
-                case undefined:
-                    lastVal = hat;
-                    return { value: hat, done: false };
-                    case hat:
-                        lastVal = boots;
-                        return { value: boots, done: false };
-                    case boots:
-                        lastVal = umbrella;
-                        return { value: umbrella, done: false };
-                    case umbrella:
-                        return { value: undefined, done: true };
-
-            }
-        }
-    }
+function* createProductIterator() {
+    yield new Product("Hat", 100);
+    yield new Product("Boots", 100);
+    yield new Product("Umbrella", 23);
 }
 
 let iterator = createProductIterator();
@@ -51,9 +32,10 @@ while (!result.done) {
     result = iterator.next();
 }
 
-// The createProductIterator function returns an object that defines a next function.
-// Each time the next method is called, a different Product object is returned, once 
-// the set of objects has been exhausted, an object whose done property is true is 
-// returned to indicate the end of the data.
-// A while loop is used to process the iterator data, calling next after each project
-// has been processed.
+// Generators are consumed in the same way as iterators. JS runtime creates the next
+// function and executes the generator function until it reaches the yield keyword,
+// which provides a value in the sequence. Execution of the generator function continues
+// gradually each time the next function is invoked. When there are no more yield 
+// statements to execuute, an object whose done property is true is created automatically.
+// Generators can be used with the spread operator, allowing the sequence to be used as a 
+// set of function parameters or to populate an array.
