f204091cb925844abbb5486312edb7e47c37f816
OmarMWarraich <earthling986@gmail.com>
2021-05-26 08:16:54 -0400

    Change Compile Config to Remove Error

--- tools/dist/index.js
+++ tools/dist/index.js
@@ -1,3 +1,7 @@
 var printMessage = function (msg) { return console.log("Message: " + msg); };
 var message = ("Hello, TypeScript");
 printMessage(message);
+var data = new Map();
+data.set("Bob", "London");
+data.set("Alice", "Paris");
+data.forEach(function (val, key) { return console.log(key + " lives in " + val); });
