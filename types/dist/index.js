ccb76b36a40e110f6a7eaaf202b97883f3c0434e
OmarMWarraich <earthling986@gmail.com>
2021-05-26 14:52:20 -0400

    Dynamically Typed JavaScript

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello, TypeScript");
+// Understanding Static Types
+// JS is dynamically typed. JS biggest paradox that values 
+// have types instead of variables. e.g.,
+let myVar;
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = 12;
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = "Hello";
+console.log(`${myVar} = ${typeof myVar}`);
+myVar = true;
+console.log(`${myVar} = ${typeof myVar}`);
