0d3a60691717941c1fa1236fbd84ff3d4dd1d6fd
OmarMWarraich <earthling986@gmail.com>
2021-05-26 02:29:39 -0400

    Use Inheritance in Classes

--- primer2/index.js
+++ primer2/index.js
@@ -1,4 +1,6 @@
-// Using JavaScript Classes
+// Using inheritance in Classes
+// Classes can inherit features using the extends keyword and invoke the superclass
+// constructor and mwthods using the super keyword.
 
 class Product {
     constructor(name, price) {
@@ -11,14 +13,49 @@ class Product {
     }
 }
 
-let hat = new Product("Hat", 100);
-let boots = new Product("Boots", 100);
+class TaxedProduct extends Product {
+
+    constructor(name, price, taxRate = 1.2) {
+        super(name, price);
+        this.taxRate = taxRate;
+    }
+
+    getPriceIncTax() {
+        return Number(this.price) * this.taxRate;
+    }
+
+    toString() {
+        let chainResult = super.toString();
+        return `${chainResult}, Tax: ${this.getPriceIncTax()}`;
+    }
+}
+
+let hat = new TaxedProduct("Hat", 100);
+let boots = new TaxedProduct("Boots", 100, 1.3)
+
 
 console.log(hat.toString());
 console.log(boots.toString());
 
-// Classes are defined with the class keyword, followed by a name for the class. 
-// Objects are created from classed using the new keyword.
-// The JS runtime creates a new object and invokes the class constructor function,
-// which recieves the new object through the this value and which is responsible
-// for defining the object's own properties. 
+// A class declares its superclass using the extends keyword. TaxedProduct class uses the
+// extend keyword to inherit from the Product class. The super keyword is used in the 
+// constructor to invoke the superclass constructor, which is equivalent to chaining
+// constructor functions.
+
+// constructor(name, price, taxRate = 1.2) {
+//     super(name, price);
+//     this.taxRate= taxRate;
+// }
+
+// The super keyword must be used before the this keyword and is generally used in the
+// first statement in the constructor. The super keyword can also be used to access 
+// superclass properties and methods like this.
+
+//toString() {
+//    let chainResult = super.toString();
+//    return `${chainResult}, Tax: ${this.getPriceIncTax()}`;
+//}
+
+// The toString method defined by the TaxedProduct class invoked the superclass's toString
+// method, which is equivalent to overriding prototype methods.
+
