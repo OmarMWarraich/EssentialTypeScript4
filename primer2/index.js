ef5777a39950a4acc86a9dce97738624fd4b800b
OmarMWarraich <earthling986@gmail.com>
2021-05-26 06:23:46 -0400

    Use JavaScript Module

--- primer2/index.js
+++ primer2/index.js
@@ -1,7 +1,4 @@
-// Storing Data by Index
-
-// JS also provides Set, which stores data by index but has performance optimizations 
-// and --most usefully--stores only unique values.
+import calcTax from './tax';
 
 class Product {
     constructor(name, price) {
@@ -9,25 +6,10 @@ class Product {
         this.name = name;
         this.price = price;
     }
-
 }
 
 let product = new Product("Hat", 100);
+let taxedPrice = calcTax(product.price);
+console.log(`Name: ${ product.name }, Taxed Price: ${taxedPrice}`);
 
-let productArray = [];
-let productSet = new Set();
 
-for (let i = 0; i < 5; i++) {
-    productArray.push(product);
-    productSet.add(product);
-}
-
-console.log(`Array length: ${productArray.length}`);
-console.log(`Set size: ${productSet.size}`);
-
-// adds the same product object five times to an array and a Set and then prints how
-// many items each contains.
-
-// For many projects, the need to allow or prevent duplicate values is the reason to 
-// choose between an array and a Set. The API provided by Set provides comparable
-// features to working with an array.
---
+++ primer2/tax.js
@@ -0,0 +1,3 @@
+export default function(price) {
+    return Number(price) * 1.2;
+}
