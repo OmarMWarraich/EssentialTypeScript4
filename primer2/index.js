c167a1912f94106d5ae20781dd92934bb1feaf9a
OmarMWarraich <earthling986@gmail.com>
2021-05-26 09:27:21 +0500

    Inspecting and Modifying an Object's Prototype and Comparing Prototypes

--- primer2/index.js
+++ primer2/index.js
@@ -1,3 +1,15 @@
+// Inspecting and Modifying an Object's inheritance
+// Object is the prototype for most objects, but it also provides methods that are used directly, rather than
+// through inheritance, and that can be used to get information about prototypes.
+
+// Useful Object Methods
+
+//               Name                                  Description
+
+//          getPrototypeOf                    This method returns an object's prototype.
+//          setPrototypeOf                    This method changes the prototye of an object.
+//          getOwnPropertyNames               This method returns the names of an object's own properties.
+
 let hat = {
     name: "Hat",
     price: 100,
@@ -5,6 +17,22 @@ let hat = {
         return Number(this.price) * 1.2;
     }
 };
+
+let boots = {
+    name: "Boots",
+    price: 100,
+    getPriceIncTax() {
+        return Number(this.price) * 1.2;
+    }
+}
+
+let hatPrototype = Object.getPrototypeOf(hat);
+console.log(`Hat Prototype: ${hatPrototype}`);
+
+let bootsPrototype = Object.getPrototypeOf(boots);
+console.log(`Boots Prototype: ${bootsPrototype}`);
+
+console.log(`Common prototype: ${ hatPrototype === bootsPrototype}`)
 
 console.log(`Hat: ${hat.price}, ${hat.getPriceIncTax()}`);
 console.log(`toString: ${hat.toString()}`);
