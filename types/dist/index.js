88f1c97dcc2448d77c18c271b3c74c3b6484d256
OmarMWarraich <earthling986@gmail.com>
2021-05-26 18:04:29 -0400

    Understanding the Never Type

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,4 +1,8 @@
-// Type Guarding in a switch Statement.
+// Understanding the Never Type
+// TS provides the never type for situations where a type guard 
+// has dealt with all of the possible types for a value. 
+// Once all the possible values have been handled, the compiler
+// will only allow a value to be assigned to the never type.
 function calculateTax(amount, format) {
     const calcAmount = amount * 1.2;
     return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
@@ -11,4 +15,12 @@ switch (typeof taxValue) {
     case "string":
         console.log(`String Value: ${taxValue.charAt(0)}`);
         break;
+    default:
+        let value = taxValue;
+        console.log(`Unexpected type for value: ${value}`);
 }
+// Something has gone wrong if execution reaches the default 
+// clause of the switch statement, and TS provides the never
+// type to ensure that you cant accidentally use a value once
+// type guards have been used to exhaustively narrow a value 
+// to all of its possible types.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,4 +1,9 @@
-// Type Guarding in a switch Statement.
+// Understanding the Never Type
+
+// TS provides the never type for situations where a type guard 
+// has dealt with all of the possible types for a value. 
+// Once all the possible values have been handled, the compiler
+// will only allow a value to be assigned to the never type.
 
 function calculateTax(amount: number, format: boolean): string | number {
     const calcAmount = amount * 1.2;
@@ -14,4 +19,13 @@ switch (typeof taxValue) {
     case "string":
         console.log(`String Value: ${taxValue.charAt(0)}`);
         break;
+    default: 
+        let value: never = taxValue;
+        console.log(`Unexpected type for value: ${value}`);
 }
+
+// Something has gone wrong if execution reaches the default 
+// clause of the switch statement, and TS provides the never
+// type to ensure that you cant accidentally use a value once
+// type guards have been used to exhaustively narrow a value 
+// to all of its possible types.
