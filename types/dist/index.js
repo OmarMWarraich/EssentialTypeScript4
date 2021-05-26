2d79331d5e578e9c4bf479fb67474f9ef64fec25
OmarMWarraich <earthling986@gmail.com>
2021-05-26 15:08:26 -0400

    Use Type Annotation

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,14 +1,13 @@
 "use strict";
-// Dynamic Types offer flexibility, but they can also lead 
-// to problems. Here a fn with a set of statements invokn it.
+// Create a Static Type with a Type Annotation
+// TS static type feature makes type assumptions explicit & 
+// allows the compiler to report an error when different data
+// types are used. Static types are defined using type
+// annotations.
+// Using a Type Annotation
 function calculateTax(amount) {
     return amount * 1.2;
 }
 console.log(`${12} = ${calculateTax(12)}`);
 console.log(`${"Hello"} = ${calculateTax("Hello")}`);
 console.log(`${true} = ${calculateTax(true)}`);
-// Easy to understand the function's assumption about its 
-// parameter type when you can see the code next to the 
-// statements that use it, but it's much harder when the
-// function has been written by another programmer and is
-// deep inside a complex project or package.
