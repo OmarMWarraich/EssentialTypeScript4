a3da9e8e99c83f4cc0f5f02dc870a681c2abb0ff
OmarMWarraich <earthling986@gmail.com>
2021-05-26 09:33:41 +0500

    Change a Prototype Property

--- primer2/index.js
+++ primer2/index.js
@@ -27,17 +27,8 @@ let boots = {
 }
 
 let hatPrototype = Object.getPrototypeOf(hat);
-console.log(`Hat Prototype: ${hatPrototype}`);
-
-let bootsPrototype = Object.getPrototypeOf(boots);
-console.log(`Boots Prototype: ${bootsPrototype}`);
-
-console.log(`Common prototype: ${ hatPrototype === bootsPrototype}`)
-
-console.log(`Hat: ${hat.price}, ${hat.getPriceIncTax()}`);
-console.log(`toString: ${hat.toString()}`);
-
-// the first console.log statement recieves  template string that includes the price property, which is one of 
-// the hat's own properties. The statement invokes the toString method. None of the hat object's own properties
-// is named toString, so the JS runtime turns to the hat Objects' prototype, which is Object and which does 
-// provide a property named toString.
+hatPrototype.toString = function(){
+    return `toString: Name: ${this.name}, Price: ${this.price}`;
+}
+console.log(hat.toString());
+console.log(boots.toString());
