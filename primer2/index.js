4c989d05b96637cffe67aed24632106f3c64edfb
OmarMWarraich <earthling986@gmail.com>
2021-05-26 06:34:09 -0400

    Export and Import Default and Named Features

--- primer2/index.js
+++ primer2/index.js
@@ -1,4 +1,4 @@
-import {calculateTax} from './tax';
+import calcTaxandSum, {calculateTax} from './tax';
 
 class Product {
     constructor(name, price) {
@@ -12,4 +12,7 @@ let product = new Product("Hat", 100);
 let taxedPrice = calculateTax(product.price);
 console.log(`Name: ${ product.name }, Taxed Price: ${taxedPrice}`);
 
+let products = [new Product("Gloves", 23), new Product("Boots", 100)]
+let totalPrice = calcTaxandSum(...products.map(p => p.price));
+console.log(`Total Price: ${ totalPrice.toFixed(2)}`);
 
