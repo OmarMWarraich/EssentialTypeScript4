229ffb74405dae17447d46ead6f99245b99c4ad5
OmarMWarraich <earthling986@gmail.com>
2021-05-26 08:14:52 -0400

    Error TS2583: Cannot Find nameMap

--- tools/src/index.ts
+++ tools/src/index.ts
@@ -3,3 +3,8 @@ let printMessage = (msg: string): void => console.log(`Message: ${ msg }`);
 let message = ("Hello, TypeScript");
 
 printMessage(message);
+
+let data = new Map();
+data.set("Bob", "London");
+data.set("Alice", "Paris");
+data.forEach((val, key) => console.log(`${ key } lives in ${val}`));
