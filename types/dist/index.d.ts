a143a1fb41fb3d942a77666a0e55067c611d4bfd
OmarMWarraich <earthling986@gmail.com>
2021-05-26 15:47:17 -0400

    Use Implicitly Defined Any Types

--- types/dist/index.d.ts
+++ types/dist/index.d.ts
@@ -2,5 +2,4 @@ declare function calculateTax(amount: any): any;
 declare let price: number;
 declare let taxAmount: any;
 declare let halfShare: number;
-declare let newResult: any;
-declare let myNumber: number;
+declare let personVal: any;
--- types/dist/index.js
+++ types/dist/index.js
@@ -1,18 +1,21 @@
-// Assigning the any Type
+// Using Implicitly Defined Any Types
+// The TS compiler will use any when it is assigning types
+// implicitly and cannot identify a more specific type to
+// use. This makes it easier to selectively apply TypeScript
+// in an existing JS project and can simplify working with 
+// 3rd party JS pkgs.
 function calculateTax(amount) {
     return `$${(amount * 1.2).toFixed(2)}`;
 }
 let price = 100;
 let taxAmount = calculateTax(price);
 let halfShare = taxAmount / 2;
+let personVal = calculateTax("Bob");
 console.log(`Price: ${price}`);
 console.log(`Full Amount in tax: ${taxAmount}`);
 console.log(`Half Share: ${halfShare}`);
-let newResult = calculateTax(200);
-let myNumber = newResult;
-console.log(`Number value: ${myNumber.toFixed(2)}`);
-// The compiler trusts that the any value can be treated as a 
-// number, which means a type mismatch occurs at runtime. This 
-// any type allows full use of the JS type features, which can
-// be useful but yield unexpected results when types are 
-// coerced automatically at runtime.
+console.log(`Name: ${personVal}`);
+// The compiler will use an implicit any for the function 
+// parameter because it isnt able to determine a better type
+// to use, which is why no compiler error will be reported
+// when the function is invoked with a string argument.
