b4ab9be7e9753b1fed40cb434e502ef43e93f5cb
OmarMWarraich <earthling986@gmail.com>
2021-05-26 08:07:02 -0400

    Use Modern Features

--- tools/dist/index.js
+++ tools/dist/index.js
@@ -1,5 +1,3 @@
-function printMessage(msg) {
-    console.log(`Message: ${msg}`);
-}
-printMessage("Hello, TypeScript");
-printMessage("it is sunny today");
+var printMessage = function (msg) { return console.log("Message: " + msg); };
+var message = ("Hello, TypeScript");
+printMessage(message);
--- tools/src/index.ts
+++ tools/src/index.ts
@@ -1,6 +1,5 @@
-function printMessage(msg: string): void {
-    console.log(`Message: ${ msg }`);
-}
+let printMessage = (msg: string): void => console.log(`Message: ${ msg }`);
 
-printMessage("Hello, TypeScript");
-printMessage("it is sunny today");
+let message = ("Hello, TypeScript");
+
+printMessage(message);
