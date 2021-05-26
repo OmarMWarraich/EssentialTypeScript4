dc1d483172b8f532a6a10e2d5bbfdf4d63657fe1
OmarMWarraich <earthling986@gmail.com>
2021-05-26 16:08:32 -0400

    Declare Union Types Explicitly

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,23 +1,14 @@
-// Using Type Unions
-// At one end of the type safety spectrum is the any feature,
-// which allows complete freedom. At the other end of the 
-// spectrum are type annotations for single types, which narrows
-// the range of allowable values. Between these two extremes,
-// TS provides type unions, which specify a set of types.
+// Declaring Union Types Explicitly
 function calculateTax(amount, format) {
     const calcAmount = amount * 1.2;
     return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
 }
 let taxNumber = calculateTax(100, false);
 let taxString = calculateTax(100, true);
-// The type returned by the calculateTax function is the
-// union of the string and number types which is defined using
-// the bar character between type names. The union can combine
-// as many types as needed.
-//Defining a Type Union
-// It is important to understand that a type union is handled 
-// as a type in its own right, whose features are the intersection
-// of the individual types. This means that the type of the 
-// taxNumber variable is string | number and not number even
-// though the calculateTax function returns a number when
-// the boolean argument is false. 
+console.log(`Number Value: ${taxNumber.toFixed(2)}`);
+console.log(`String Value: ${taxString.charAt(0)}`);
+// Only the props and meths defined by all the types in union
+// can be used in the union, as above only common type is toString
+// Therefore, the other methods defined by the string and number
+// types cannot be used and the use of the toFixed and charAt
+// methods produces compiler messages. errorTS2339
