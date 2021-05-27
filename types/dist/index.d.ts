0f4fe7a64346fb2f32aca7fce05c4516db58ce21
OmarMWarraich <earthling986@gmail.com>
2021-05-27 06:02:11 -0400

    Avoid Problems with Empty Arrays

--- types/dist/index.d.ts
+++ types/dist/index.d.ts
@@ -1,4 +1,4 @@
 declare function calculateTax(amount: number): number;
 declare function writePrice(product: string, price: number): void;
-declare let prices: (string | number)[];
+declare let prices: any[];
 declare let names: string[];
--- types/dist/index.js
+++ types/dist/index.js
@@ -1,19 +1,26 @@
-// Mixing Array Types
-// compiler infers array types using the values used to populate the
-// array when it is created. This can lead to type errors if the 
-// values used to populate an array are accidentally mixed.
+// Avoiding Problems with Empty Arrays
+// Another reason for using type annotations for arrays is that the
+// compiler will infer the type any for arrays that are created
+// empty.
 function calculateTax(amount) {
     return amount * 1.2;
 }
 function writePrice(product, price) {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
-let prices = [100, 75, 42, "20"];
+let prices = [];
+prices.push(...[100, 75, 42, "20"]);
 let names = ["Hat", "Gloves", "Umbrella", "Sunglasses"];
 prices.forEach((price, index) => {
     writePrice(names[index], calculateTax(price));
 });
-// The new value used to initialize the prie array causes the following
-// error when code is compiled. This
-// ts2345: Arg of type 'string' | 'number' not assignable to parameter
-// of type 'number'.
+// There are no intial values for the compiler to use when selecting
+// the type for the prices array. The only option available to the
+// compiler is to use any since it has no other information to 
+// work with which can be seen in inde.d.ts in dist folder.
+// The effect of allowing the compiler to infer the type of the 
+// empty array is to create a gap in the type checking process.
+// The code works because the JS multiplication operator coerces
+// string values to number values automatically. This can be useful
+// behaviour, but it is likely to be used accidentally, and it is 
+// for this reason that you should use explicit types.
