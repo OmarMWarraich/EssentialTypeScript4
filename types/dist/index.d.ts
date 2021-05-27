84cfc793cace710859823863f761dffa1811173b
OmarMWarraich <earthling986@gmail.com>
2021-05-27 05:48:26 -0400

    Use Inferred Typing for Arrays

--- types/dist/index.d.ts
+++ types/dist/index.d.ts
@@ -1,4 +1,4 @@
 declare function calculateTax(amount: number): number;
 declare function writePrice(product: string, price: number): void;
-declare let prices: Array<number>;
+declare let prices: number[];
 declare let names: string[];
--- types/dist/index.js
+++ types/dist/index.js
@@ -1,15 +1,22 @@
-// The Array Syntax
+// Using Inferred Typings for Arrays
+// TS compiler is adept at inferring types automatically, and the
+// same example can be expressed without type annotations.
 function calculateTax(amount) {
     return amount * 1.2;
 }
 function writePrice(product, price) {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
-// let prices: number[] = [100, 75, 42];
-// is equivalent to the following code: 
 let prices = [100, 75, 42];
 let names = ["Hat", "Gloves", "Umbrella"];
 prices.forEach((price, index) => {
     writePrice(names[index], calculateTax(price));
 });
-// The problem with this syntax is that it cant be used in TSX.
+// The compiler can determine the array types based on the set of
+// values that are assigned when the arrays are initialized and it
+// uses the inferred types to follow through to the forEach method.
+// The compiler is skilled at inferring types, but if you dont get
+// the results you expect, you can expect the files that the compiler
+// emits when the declaration option is eabled. This option generates
+// type declaration files, which are used to provide type information
+// when a package is used in another TS project.
