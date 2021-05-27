c6ceb870b126adf03a73589664b87d22d55fef4c
OmarMWarraich <earthling986@gmail.com>
2021-05-27 06:31:24 -0400

    Define a Tuple

--- types/dist/index.d.ts
+++ types/dist/index.d.ts
@@ -1,4 +1,4 @@
 declare function calculateTax(amount: number): number;
 declare function writePrice(product: string, price: number): void;
-declare let prices: any[];
-declare let names: string[];
+declare let hat: [string, number];
+declare let gloves: [string, number];
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,8 +1,9 @@
-// Avoiding Problems with Empty Arrays
+// Working with Tuples
 
-// Another reason for using type annotations for arrays is that the
-// compiler will infer the type any for arrays that are created
-// empty.
+// Basic tuples are fixed-length arrays where each element in the
+// array can have a different type. Tuples are a data structure
+// that is provided by the TS compiler implemented using regular JS
+// arrays in the compiled code. 
 
 function calculateTax(amount: number): number {
     return amount * 1.2;
@@ -12,23 +13,19 @@ function writePrice(product: string, price: number): void {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
 
-let prices = [];
-prices.push(...[100, 75, 42, "20"]);
+let hat: [string, number] = ["Hat", 100];
+let gloves: [string, number] = ["Gloves", 75];
 
-let names = ["Hat", "Gloves", "Umbrella", "Sunglasses"];
+writePrice(hat[0], hat[1]);
+writePrice(gloves[0], gloves[1]);
 
-prices.forEach((price, index) => {
-    writePrice(names[index], calculateTax(price));
-})
+// Tuples are defined using square brackets containing the types for
+// each element, spearated by commas. 
 
-// There are no intial values for the compiler to use when selecting
-// the type for the prices array. The only option available to the
-// compiler is to use any since it has no other information to 
-// work with which can be seen in inde.d.ts in dist folder.
-
-// The effect of allowing the compiler to infer the type of the 
-// empty array is to create a gap in the type checking process.
-// The code works because the JS multiplication operator coerces
-// string values to number values automatically. This can be useful
-// behaviour, but it is likely to be used accidentally, and it is 
-// for this reason that you should use explicit types.
+// Tuples must be defined with type annotations; otherwise, the 
+// compiler will assume that a regular array with a type that is the
+// union of each value used during initialization. Without the type
+// annotation, the compiler would assume that the type of the value
+// assigned to the hat variable is [string | number], which would 
+// denote a variable-length array in which every element can be 
+// either a string or number value.
