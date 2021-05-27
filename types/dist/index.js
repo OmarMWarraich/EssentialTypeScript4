314beb5a4f1176ec39dca759077a2cb22b9526fc
OmarMWarraich <earthling986@gmail.com>
2021-05-27 04:09:19 -0400

    Disable Implicit Returns-errorTS7030-Not all code paths return a value

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,4 +1,13 @@
-// Allow a Null Parameter Value
+// Disable Implicit Returns
+// JS has an unusually relaced approach to function results, such
+// that a function will return undefined for any path through the
+// funtion's code that doesnt reach a statement with the return 
+// keyword, which is known as the implicit return feature.
+// The Type Guard used to filter out null values means that there 
+// is a path through the function's code that doesnt reach a 
+// return statement and so the function will return a number if 
+// the amount parameter isnt null and will return undefined if 
+// the amount parameter is null. 
 function calculateTax(amount, discount = 0, ...extraFees) {
     if (amount != null) {
         return (amount * 1.2) - (discount)
@@ -7,8 +16,7 @@ function calculateTax(amount, discount = 0, ...extraFees) {
 }
 let taxValue = calculateTax(null, 0);
 console.log(`Tax value: ${taxValue}`);
-// A Type Guard is required to prevent the null value from being
-// used with the multiplication operator. This can feel like an
-// arduoud process when you start using TypeScript, but restricting
-// nullable parameters can flush out problems that would othewise 
-// produce unexpected results at runtime.
+// When the noImplicitReturns setting is true, the compiler will
+// report an error when there are paths through functions that dont 
+// explicitly produce a result with the result keyword or throw an
+// error. Save the change to the tsconfig.json file.
