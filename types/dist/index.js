0b868643579d142b3dbbb1847cc6f85e81682ee2
OmarMWarraich <earthling986@gmail.com>
2021-05-28 08:23:42 -0400

    Define a Class

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,9 +1,27 @@
 "use strict";
+// Define a Class
 Object.defineProperty(exports, "__esModule", { value: true });
 const dataTypes_1 = require("./dataTypes");
 let people = [new dataTypes_1.Person("Bob Smith", "London"),
     new dataTypes_1.Person("Dora Peters", "New York")];
 let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
-[...people, ...products].forEach(item => console.log(`Item: ${item.name}`));
-// The listing uses an import statement to declare dependencies on the Person and Product
-// classes defined in the dataTypes module. 
+class PeopleCollection {
+    constructor(initialItems) {
+        this.items = [];
+        this.items.push(...initialItems);
+    }
+    getNames() {
+        return this.items.map(item => item.name);
+    }
+    getItem(index) {
+        return this.items[index];
+    }
+}
+let peopleData = new PeopleCollection(people);
+console.log(`Names: ${peopleData.getNames().join(', ')}`);
+let firstPerson = peopleData.getItem(0);
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
+// The PeopleCollection class operates on Person objects, which are provided via the 
+// constructor or the add method. The getNames method returns an array containing the
+// name value of each Person object, and the getItem method allows a Person object to be
+// retrieved using an index. 
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,10 +1,34 @@
+// Define a Class
+
 import { Person, Product } from './dataTypes';
 
 let people = [new Person("Bob Smith", "London"),
     new Person("Dora Peters", "New York")];
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 
-[...people, ...products].forEach(item => console.log(`Item: ${item.name}`));
+class PeopleCollection {
+    private items: Person[] = [];
 
-// The listing uses an import statement to declare dependencies on the Person and Product
-// classes defined in the dataTypes module. 
+    constructor(initialItems: Person[]) {
+        this.items.push(...initialItems);
+    }
+
+    getNames(): string[] {
+        return this.items.map(item => item.name);
+    }
+
+    getItem(index: number): Person {
+        return this.items[index];
+    }
+}
+
+let peopleData = new PeopleCollection(people);
+
+console.log(`Names: ${peopleData.getNames().join(', ')}`);
+let firstPerson = peopleData.getItem(0);
+console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
+
+// The PeopleCollection class operates on Person objects, which are provided via the 
+// constructor or the add method. The getNames method returns an array containing the
+// name value of each Person object, and the getItem method allows a Person object to be
+// retrieved using an index. 
