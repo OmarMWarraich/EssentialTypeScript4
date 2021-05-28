f3fb47f5c560fef36c68db08e9b31598d05727fa
OmarMWarraich <earthling986@gmail.com>
2021-05-28 09:26:02 -0400

    Define Multiple Type Parameters

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,48 +1,30 @@
 "use strict";
-// Constraining Generic Types Using a Shape
+// Defining Multiple Type Parameters
 Object.defineProperty(exports, "__esModule", { value: true });
-// Using a type union to constrain generic type parameters is useful, but the union must
-// be extended for each new type that is required. An alternative approach is to use a
-// shape to constrain the type parameter, which will allow only the properties that the 
-// generic class relies on.
-// Using a Shape Type
+// A class can define multiple type parameters. Below a second parameter added to the
+// DataCollection<T> class and used for data correlation. Methods no longer reqd removed.
+// Defining Another Generic Type Parameter
 const dataTypes_1 = require("./dataTypes");
 let people = [new dataTypes_1.Person("Bob Smith", "London"),
     new dataTypes_1.Person("Dora Peters", "New York")];
 let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
 let cities = [new dataTypes_1.City("London", 8136000), new dataTypes_1.City("Paris", 2141000)];
-// type dataType = Person | Product;
 class DataCollection {
     constructor(initalItems) {
         this.items = [];
         this.items.push(...initalItems);
     }
-    add(newItem) {
-        this.items.push(newItem);
-    }
-    getNames() {
-        return this.items.map(item => item.name);
-    }
-    getItem(index) {
-        return this.items[index];
+    collate(targetData, itemProp, targetProp) {
+        let results = [];
+        this.items.forEach(item => {
+            let match = targetData.find(d => d[targetProp] === item[itemProp]);
+            if (match !== undefined) {
+                results.push({ ...match, ...item });
+            }
+        });
+        return results;
     }
 }
 let peopleData = new DataCollection(people);
-console.log(`Person Names: ${peopleData.getNames().join(', ')}`);
-let firstPerson = peopleData.getItem(0);
-//if (firstPerson instanceof Person) {
-console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
-//}
-let productData = new DataCollection(products);
-console.log(`Product Names: ${productData.getNames().join(', ')}`);
-let firstProduct = productData.getItem(0);
-console.log(`First Product: ${firstProduct.name}, ${firstProduct.price}`);
-let cityData = new DataCollection(cities);
-console.log(`City Names: ${cityData.getNames().join(', ')}`);
-// Above specified shape tells the compiler that the DataCollection<T> class can be 
-// instantiated using any type that has a name property returning a string. This allows
-// DataCollection objects to be created to deal with Person, Product and City objects 
-// without requiring individual types to be specified.
-// Tip: Generic type parameters can also be constrained using type aliases and interfaces.
-// also possible to constrain generic types to those that define a specific constructor
-// shape which is done with the extendor keywords.
+let collatedData = peopleData.collate(cities, "city", "name");
+collatedData.forEach(c => console.log(`${c.name}, ${c.city}, ${c.population}`));
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,11 +1,9 @@
-// Constraining Generic Types Using a Shape
+// Defining Multiple Type Parameters
 
-// Using a type union to constrain generic type parameters is useful, but the union must
-// be extended for each new type that is required. An alternative approach is to use a
-// shape to constrain the type parameter, which will allow only the properties that the 
-// generic class relies on.
+// A class can define multiple type parameters. Below a second parameter added to the
+// DataCollection<T> class and used for data correlation. Methods no longer reqd removed.
 
-// Using a Shape Type
+// Defining Another Generic Type Parameter
 
 import { City,Person, Product } from './dataTypes';
 
@@ -14,10 +12,7 @@ let people = [new Person("Bob Smith", "London"),
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 let cities = [new City("London", 8136000), new City("Paris", 2141000)];
 
-
-// type dataType = Person | Product;
-
-class DataCollection<T extends {name: string}> {
+class DataCollection<T extends {name: string}, U> {
 
     private items: T[] = [];
 
@@ -25,41 +20,20 @@ class DataCollection<T extends {name: string}> {
         this.items.push(...initalItems);
     }
 
-    add(newItem: T) {
-        this.items.push(newItem);
-    }
-
-    getNames(): string[] {
-        return this.items.map(item => item.name);
-    }
-
-    getItem(index: number): T {
-        return this.items[index];
+    collate(targetData: U[], itemProp: string, targetProp: string): (T & U)[] {
+        let results = [];
+        this.items.forEach(item => {
+            let match = targetData.find(d => d[targetProp] === item[itemProp]);
+            if (match !== undefined) {
+                results.push({ ...match, ...item });
+            }
+        });
+        return results;
     }
 }
 
-let peopleData = new DataCollection<Person>(people);
-
-console.log(`Person Names: ${peopleData.getNames().join(', ')}`);
-let firstPerson = peopleData.getItem(0);
-//if (firstPerson instanceof Person) {
-console.log(`First Person: ${firstPerson.name}, ${firstPerson.city}`);
-//}
-
-let productData = new DataCollection<Product>(products);
-console.log(`Product Names: ${productData.getNames().join(', ')}`);
-let firstProduct = productData.getItem(0);
-console.log(`First Product: ${firstProduct.name}, ${firstProduct.price}`);
-
-let cityData = new DataCollection<City>(cities);
-console.log(`City Names: ${cityData.getNames().join(', ')}`);
+let peopleData = new DataCollection<Person, City>(people);
+let collatedData = peopleData.collate(cities, "city", "name");
+collatedData.forEach(c => console.log(`${c.name}, ${c.city}, ${c.population}`));
 
 
-// Above specified shape tells the compiler that the DataCollection<T> class can be 
-// instantiated using any type that has a name property returning a string. This allows
-// DataCollection objects to be created to deal with Person, Product and City objects 
-// without requiring individual types to be specified.
-
-// Tip: Generic type parameters can also be constrained using type aliases and interfaces.
-// also possible to constrain generic types to those that define a specific constructor
-// shape which is done with the extendor keywords.
