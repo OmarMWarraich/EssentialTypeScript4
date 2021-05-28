8f1a177ebbcaecbc25561d078a6a681b3675abf1
OmarMWarraich <earthling986@gmail.com>
2021-05-28 02:18:59 -0400

    Using Interfaces

--- types/dist/index.d.ts
+++ types/dist/index.d.ts
@@ -1,24 +1,21 @@
-declare abstract class Person {
-    id: string;
+interface Person {
     name: string;
-    city: string;
-    constructor(id: string, name: string, city: string);
     getDetails(): string;
-    abstract getSpecificDetails(): string;
 }
-declare class Employee extends Person {
+declare class Employee implements Person {
     readonly id: string;
     name: string;
     private dept;
     city: string;
     constructor(id: string, name: string, dept: string, city: string);
-    getSpecificDetails(): string;
+    getDetails(): string;
 }
-declare class Customer {
+declare class Customer implements Person {
     readonly id: string;
     name: string;
     city: string;
     creditLimit: number;
     constructor(id: string, name: string, city: string, creditLimit: number);
+    getDetails(): string;
 }
-declare let data: (Person | Customer)[];
+declare let data: Person[];
