f94bc1ed78d7757c13ff777c2a7dd06f47c772dd
OmarMWarraich <earthling986@gmail.com>
2021-05-28 09:39:37 -0400

    Apply Type Parameter to a Method

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,14 +1,20 @@
 "use strict";
-// Defining Multiple Type Parameters
+// Applying a Type Parameter to a Method
 Object.defineProperty(exports, "__esModule", { value: true });
-// A class can define multiple type parameters. Below a second parameter added to the
-// DataCollection<T> class and used for data correlation. Methods no longer reqd removed.
-// Defining Another Generic Type Parameter
+// The second type parameter isnt as flexible as it could be because it requires the 
+// data type used by the collate method to be specified when the DataCollection object
+// is created, meaning that's the only data type that can be used with that method.
+// When a type is used by only one method, the type parameter can be moved from the class
+// declaration and applied directly to the method, allowing a different type to be 
+// specified each time the method is invoked.
+// Applying a Type Parameter to a Method
 const dataTypes_1 = require("./dataTypes");
 let people = [new dataTypes_1.Person("Bob Smith", "London"),
     new dataTypes_1.Person("Dora Peters", "New York")];
 let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
 let cities = [new dataTypes_1.City("London", 8136000), new dataTypes_1.City("Paris", 2141000)];
+let employees = [new dataTypes_1.Employee("Bob Smith", "Sales"),
+    new dataTypes_1.Employee("Alice Jones", "Sales")];
 class DataCollection {
     constructor(initalItems) {
         this.items = [];
@@ -28,3 +34,9 @@ class DataCollection {
 let peopleData = new DataCollection(people);
 let collatedData = peopleData.collate(cities, "city", "name");
 collatedData.forEach(c => console.log(`${c.name}, ${c.city}, ${c.population}`));
+let empData = peopleData.collate(employees, "name", "name");
+empData.forEach(c => console.log(`${c.name}, ${c.city}, ${c.role}`));
+// The type parameter U is applied directly to the collate method, allowing a type to
+// provided when the method is invoked.
+// The method's type parameter allows the collate method to be invoked using City objects
+// and then invoked again with Employee objects.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,18 +1,27 @@
-// Defining Multiple Type Parameters
+// Applying a Type Parameter to a Method
 
-// A class can define multiple type parameters. Below a second parameter added to the
-// DataCollection<T> class and used for data correlation. Methods no longer reqd removed.
+// The second type parameter isnt as flexible as it could be because it requires the 
+// data type used by the collate method to be specified when the DataCollection object
+// is created, meaning that's the only data type that can be used with that method.
 
-// Defining Another Generic Type Parameter
+// When a type is used by only one method, the type parameter can be moved from the class
+// declaration and applied directly to the method, allowing a different type to be 
+// specified each time the method is invoked.
 
-import { City,Person, Product } from './dataTypes';
+// Applying a Type Parameter to a Method
+
+import { City,Person, Product, Employee } from './dataTypes';
 
 let people = [new Person("Bob Smith", "London"),
     new Person("Dora Peters", "New York")];
 let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 let cities = [new City("London", 8136000), new City("Paris", 2141000)];
+let employees = [new Employee("Bob Smith", "Sales"),
+    new Employee("Alice Jones", "Sales")];
 
-class DataCollection<T extends {name: string}, U> {
+
+
+class DataCollection<T extends {name: string}> {
 
     private items: T[] = [];
 
@@ -20,7 +29,7 @@ class DataCollection<T extends {name: string}, U> {
         this.items.push(...initalItems);
     }
 
-    collate(targetData: U[], itemProp: string, targetProp: string): (T & U)[] {
+    collate<U>(targetData: U[], itemProp: string, targetProp: string): (T & U)[] {
         let results = [];
         this.items.forEach(item => {
             let match = targetData.find(d => d[targetProp] === item[itemProp]);
@@ -32,8 +41,16 @@ class DataCollection<T extends {name: string}, U> {
     }
 }
 
-let peopleData = new DataCollection<Person, City>(people);
-let collatedData = peopleData.collate(cities, "city", "name");
+let peopleData = new DataCollection<Person>(people);
+let collatedData = peopleData.collate<City>(cities, "city", "name");
 collatedData.forEach(c => console.log(`${c.name}, ${c.city}, ${c.population}`));
+let empData = peopleData.collate<Employee>(employees, "name", "name");
+empData.forEach(c => console.log(`${c.name}, ${c.city}, ${c.role}`));
+
+// The type parameter U is applied directly to the collate method, allowing a type to
+// provided when the method is invoked.
+
+// The method's type parameter allows the collate method to be invoked using City objects
+// and then invoked again with Employee objects.
 
 
