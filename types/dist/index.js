1d53e2345f83366fd52a5d6fa2261217d25e01ac
OmarMWarraich <earthling986@gmail.com>
2021-05-29 18:55:33 -0400

    Use an Index Type

--- types/dist/index.js
+++ types/dist/index.js
@@ -34,4 +34,26 @@ let itemByKey = productCollection.get("Hat");
 console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
 // The class has been rewritten with an additional generic type parameter,K, that is 
 // restricted to keyof T, which is the data type of the objects stored by the collection.
-// 
+// The dense chains of angle and square brackets above can be difficult to make sense
+// of when you first start using index types. Following table describes the significant 
+// type and constructor parameters and the types they are resolved to for the 
+// Collection<Product, "name"> object.
+// The SIgnificant Types Used by the Collection<T> class
+//  Name                Description
+//   T  =  type of the objects stored in the Collection calss, provided by the first
+//         generic type argument which is Product for the object created in the listing.
+//   K  =  key property name, restricted to the property names defined by T. The value for
+//         this type is provided by the second generic type argument, which is name for the
+//         object created in the listing.
+//  T[K]=  key property, obtained using the indexed acess operator which is used to specify
+//         the key type when creating the Map Object and to restrict the type for the 
+//         parameters. This is the type of the Product.name property for the object created
+//         in the listing, which is string.
+// propertyName  = key property name required as a value that can be used by the JS runtime
+//               after  the TS generic type information has been removed. For the object
+//             created in the listing, this value is name, corresponding to the generic
+//             type K.
+// The results of the index type in above example are that any property can be used to store
+// objects and that any type of object can be stored. Next commit shows the way that 
+// Collection<T, K> class is instantiated so that the proce property is used as the key. further
+// generic type arguments omitted and compiler allowed to infer the required types.
