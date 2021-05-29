fc5623f4f0573fa6e06dacb31afb258f1507e9fa
OmarMWarraich <earthling986@gmail.com>
2021-05-28 21:36:09 -0400

    Using a Collection

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,23 +1,36 @@
 "use strict";
-// Preparing for Chapter 12- Advanced Generic Types
+// Using Generic Collections
 Object.defineProperty(exports, "__esModule", { value: true });
+// TS provides support for using the JS collectiions with generic type parameters 
+// allowing a generic classs to safely use collections. 
+// The Generic Collection Types
+//            Name                     Description
+//         Map<K, V>         Map whose key type is K and whose value type is V. 
+//   ReadonlyMap<K, V>       Map that cannot be modified.
+//         Set<T>            Set whose value type is T.
+//   ReadonlySet<T>          Set that cannot be modified.
+// Using a Collection
 const dataTypes_1 = require("./dataTypes");
 let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
 class Collection {
-    constructor(items = []) {
-        this.items = items;
+    constructor(initialItems = []) {
+        this.items = new Set(initialItems);
     }
     add(...newItems) {
-        this.items.push(...newItems);
+        newItems.forEach(newItem => this.items.add(newItem));
     }
     get(name) {
-        return this.items.find(item => item.name === name);
+        return [...this.items.values()].find(item => item.name === name);
     }
     get count() {
-        return this.items.length;
+        return this.items.size;
     }
 }
 let productCollection = new Collection(products);
 console.log(`There are ${productCollection.count} products`);
 let p = productCollection.get("Hat");
 console.log(`Product: ${p.name}, ${p.price}`);
+// The Collection<T> class has been changed to Set<T> to store its items, which it does
+// by using its generic type parameter for the collection. THE TS compiler uses the type
+// parameter to prevent other data types from being added to the set, and no type guarding
+// is required when retrieving objects from the collection.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,4 +1,16 @@
-// Preparing for Chapter 12- Advanced Generic Types
+// Using Generic Collections
+
+// TS provides support for using the JS collectiions with generic type parameters 
+// allowing a generic classs to safely use collections. 
+
+// The Generic Collection Types
+//            Name                     Description
+//         Map<K, V>         Map whose key type is K and whose value type is V. 
+//   ReadonlyMap<K, V>       Map that cannot be modified.
+//         Set<T>            Set whose value type is T.
+//   ReadonlySet<T>          Set that cannot be modified.
+
+// Using a Collection
 
 import { City, Person, Product, Employee } from "./dataTypes";
 
@@ -7,19 +19,22 @@ let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 type  shapeType = { name: string };
 
 class Collection<T extends shapeType> {
+    private items: Set<T>;
 
-    constructor(private items: T[] = []) {}
+    constructor(initialItems: T[] = []) {
+        this.items = new Set<T>(initialItems);
+    }
     
     add(...newItems: T[]): void{
-        this.items.push(...newItems);
+        newItems.forEach(newItem => this.items.add(newItem));
 }
 
     get(name: string): T {
-        return this.items.find(item => item.name === name);
+        return [...this.items.values()].find(item => item.name === name);
     }
 
     get count(): number {
-        return this.items.length;
+        return this.items.size;
     }
 }
 
@@ -27,3 +42,8 @@ let productCollection: Collection<Product> = new Collection(products);
 console.log(`There are ${ productCollection.count } products`);
 let p = productCollection.get("Hat");
 console.log(`Product: ${ p.name }, ${ p.price }`);
+
+// The Collection<T> class has been changed to Set<T> to store its items, which it does
+// by using its generic type parameter for the collection. THE TS compiler uses the type
+// parameter to prevent other data types from being added to the set, and no type guarding
+// is required when retrieving objects from the collection.
