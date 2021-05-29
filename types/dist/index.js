dfb1ac11e4898399d8892d7c7bb690c72101eca9
OmarMWarraich <earthling986@gmail.com>
2021-05-28 23:21:51 -0400

    Use an Index Type for Collection<T>

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,22 +1,37 @@
 "use strict";
-// Using the Indexed Access Operator with a Generic Type
+// Using an Index Type for the Collection<T> Class
 Object.defineProperty(exports, "__esModule", { value: true });
+// Using an index type allows to change the COllection<T> class so that it can store any
+// type of object and not just those that define a name property. Below shows the change
+// to the class, which uses an index type query to restrict the propertyName constructor 
+// property to the names of the properties defined by the generic type parameter T, 
+// providing the key by which objects can be stored in the Map.
 const dataTypes_1 = require("./dataTypes");
-function getValue(item, keyname) {
-    return item[keyname];
+let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
+//type shapeType = {name: string};
+class Collection {
+    constructor(initialItems = [], propertyName) {
+        this.propertyName = propertyName;
+        this.items = new Map();
+        this.add(...initialItems);
+    }
+    add(...newItems) {
+        newItems.forEach(newItem => this.items.set(newItem[this.propertyName], newItem));
+    }
+    get(key) {
+        return this.items.get(key);
+    }
+    get count() {
+        return this.items.size;
+    }
+    [Symbol.iterator]() {
+        return this.items.values();
+    }
 }
-let p = new dataTypes_1.Product("Running Shoes", 100);
-console.log(getValue(p, "name"));
-console.log(getValue(p, "price"));
-let e = new dataTypes_1.Employee("Bob Smith", "Sales");
-console.log(getValue(e, "name"));
-console.log(getValue(e, "role"));
-// Theindexed access operator is expressed using a regular type, its keyof shape, and
-// square brackets.
-// The indexed access operator
-// The indexed access operator above, T[K], tells the compiler that the result of the
-// getValue function will have the type of the property whose name is specified by the
-// keyof type argument, leaving the compiler to determine the result types based on the
-// generic type arguments used to invoke the function. For the Product object, that means
-// a name argument will produce a string result, and a price argument will produce a 
-// number result.
+let productCollection = new Collection(products, "name");
+console.log(`There are ${productCollection.count} products`);
+let itemByKey = productCollection.get("Hat");
+console.log(`Item: ${itemByKey.name}, ${itemByKey.price}`);
+// The class has been rewritten with an additional generic type parameter,K, that is 
+// restricted to keyof T, which is the data type of the objects stored by the collection.
+// 
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,28 +1,51 @@
-// Using the Indexed Access Operator with a Generic Type
+// Using an Index Type for the Collection<T> Class
 
+// Using an index type allows to change the COllection<T> class so that it can store any
+// type of object and not just those that define a name property. Below shows the change
+// to the class, which uses an index type query to restrict the propertyName constructor 
+// property to the names of the properties defined by the generic type parameter T, 
+// providing the key by which objects can be stored in the Map.
 
 import { City, Person, Product, Employee } from "./dataTypes";
 
-function getValue<T, K extends keyof T>(item: T, keyname: K): T[K] {
-    return item[keyname];
+let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
+
+//type shapeType = {name: string};
+
+class Collection<T, K extends keyof T> implements Iterable<T> {
+    private items: Map<T[K], T>;
+
+    constructor(initialItems: T[] = [], private propertyName: K) {
+        this.items = new Map<T[K], T>();
+        this.add(...initialItems);
+    }
+
+    add(...newItems:T[]):void {
+        newItems.forEach(newItem =>
+            this.items.set(newItem[this.propertyName], newItem));
+    }
+
+    get(key: T[K]): T {
+        return this.items.get(key)
+    }
+
+    get count(): number {
+        return this.items.size;
+    }
+
+    [Symbol.iterator](): Iterator<T> {
+        return this.items.values();
+    }   
 }
 
-let p = new Product("Running Shoes", 100);
-console.log(getValue<Product, "name">(p, "name"));
-console.log(getValue(p, "price"));
+let productCollection: Collection<Product, "name">
+    = new Collection(products, "name");
+console.log(`There are ${ productCollection.count } products`);
 
-let e = new Employee("Bob Smith", "Sales");
-console.log(getValue(e, "name"));
-console.log(getValue(e, "role"));
+let itemByKey = productCollection.get("Hat");
+console.log(`Item: ${ itemByKey.name }, ${ itemByKey.price }`);
 
-// Theindexed access operator is expressed using a regular type, its keyof shape, and
-// square brackets.
+// The class has been rewritten with an additional generic type parameter,K, that is 
+// restricted to keyof T, which is the data type of the objects stored by the collection.
 
-// The indexed access operator
-
-// The indexed access operator above, T[K], tells the compiler that the result of the
-// getValue function will have the type of the property whose name is specified by the
-// keyof type argument, leaving the compiler to determine the result types based on the
-// generic type arguments used to invoke the function. For the Product object, that means
-// a name argument will produce a string result, and a price argument will produce a 
-// number result.
+// 
