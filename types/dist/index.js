192f8766d3fbd74e09607f19d58e325144b9be2a
OmarMWarraich <earthling986@gmail.com>
2021-05-28 20:41:20 -0400

    Implement an Interface

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,4 +1,39 @@
 "use strict";
-// Extending Generic Interfaces
+// Implementing a Generic Interface
 Object.defineProperty(exports, "__esModule", { value: true });
-// No output
+// When a class implements a generic interface, it must implement all the interface
+// properties and methods but it has some choices about how to deal with type parameters,
+// as described in the following sections. Some of these options are similar to those
+// used when extending generic classes and interfaces.
+// Passing on the Generic Type Parameter
+// The simplest approach is to implement the interface properties and methods without 
+// changing the type parameter, creating a generic class that directly implements the
+// interface.
+// Implementing an Interface.
+const dataTypes_1 = require("./dataTypes");
+class ArrayCollection {
+    constructor() {
+        this.items = [];
+    }
+    add(...newItems) {
+        this.items.push(...newItems);
+    }
+    get(name) {
+        return this.items.find(item => item.name === name);
+    }
+    get count() {
+        return this.items.length;
+    }
+}
+let peopleCollection = new ArrayCollection();
+peopleCollection.add(new dataTypes_1.Person("Bob Smith", "London"), new dataTypes_1.Person("Dora Peters", "New York"));
+console.log(`Collection size: ${peopleCollection.count}`);
+// The ArrayCollection<DataType> class uses the implements keyword to declare that it
+// conforms to the interface. The interface has a generic type parameter, so the 
+// ArrayCollection<DataType> class must define a compatible parameter. Since the type
+// parameter for the interface is required to have a name property, so must the parameter
+// for the class.
+// The ArrayCollection<DataType> class requires a type argument when an object is created
+// and can be operated on through the Collection<T> interface. 
+// The type argument resolves the generic type for the class and the interface it 
+// implements so that an ArrayCollection<Person> object implements the Collection<Person> interface.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,8 +1,17 @@
-// Extending Generic Interfaces
+// Implementing a Generic Interface
 
-// Generic Interfaces can be extended just like regular interfaces, and the options for
-// dealing with its type parameters are the same as when extending a generic class. Below
-// shows a set of interfaces that extend the Collection<T> interface.
+// When a class implements a generic interface, it must implement all the interface
+// properties and methods but it has some choices about how to deal with type parameters,
+// as described in the following sections. Some of these options are similar to those
+// used when extending generic classes and interfaces.
+
+// Passing on the Generic Type Parameter
+
+// The simplest approach is to implement the interface properties and methods without 
+// changing the type parameter, creating a generic class that directly implements the
+// interface.
+
+// Implementing an Interface.
 
 import { City, Person, Product, Employee } from './dataTypes';
 
@@ -15,16 +24,36 @@ interface Collection<T extends shapeType> {
     count: number;
 }
 
-interface SearchableCollection<T extends shapeType> extends Collection<T> {
-    find(name: string): T | undefined;
+class ArrayCollection<DataType extends shapeType> implements Collection<DataType> {
+    private items: DataType[] = [];
+
+    add(...newItems): void {
+        this.items.push(...newItems);
+    }
+
+    get(name: string): DataType {
+        return this.items.find(item => item.name === name);
+    }
+
+    get count(): number {
+        return this.items.length;
+    }
 }
 
-interface ProductCollection extends Collection<Product> {
-    sumPrices(): number;
-}
+let peopleCollection: Collection<Person> = new ArrayCollection<Person>();
+peopleCollection.add(new Person("Bob Smith", "London"),
+    new Person("Dora Peters", "New York"));
+console.log(`Collection size: ${peopleCollection.count}`);
 
-interface PeopleCollection<T extends Product | Employee> extends Collection<T> {
-    getNames(): string[];
-}
+// The ArrayCollection<DataType> class uses the implements keyword to declare that it
+// conforms to the interface. The interface has a generic type parameter, so the 
+// ArrayCollection<DataType> class must define a compatible parameter. Since the type
+// parameter for the interface is required to have a name property, so must the parameter
+// for the class.
 
-// No output
+// The ArrayCollection<DataType> class requires a type argument when an object is created
+// and can be operated on through the Collection<T> interface. 
+
+// The type argument resolves the generic type for the class and the interface it 
+// implements so that an ArrayCollection<Person> object implements the Collection<Person> interface.
+
