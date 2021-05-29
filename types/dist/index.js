916ec6eee39f2b07369a20f15f08e8468192626b
OmarMWarraich <earthling986@gmail.com>
2021-05-28 21:40:31 -0400

    Using a Map

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,26 +1,18 @@
 "use strict";
-// Using Generic Collections
+// Using A Map.
 Object.defineProperty(exports, "__esModule", { value: true });
-// TS provides support for using the JS collectiions with generic type parameters 
-// allowing a generic classs to safely use collections. 
-// The Generic Collection Types
-//            Name                     Description
-//         Map<K, V>         Map whose key type is K and whose value type is V. 
-//   ReadonlyMap<K, V>       Map that cannot be modified.
-//         Set<T>            Set whose value type is T.
-//   ReadonlySet<T>          Set that cannot be modified.
-// Using a Collection
 const dataTypes_1 = require("./dataTypes");
 let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
 class Collection {
     constructor(initialItems = []) {
-        this.items = new Set(initialItems);
+        this.items = new Map();
+        this.add(...initialItems);
     }
     add(...newItems) {
-        newItems.forEach(newItem => this.items.add(newItem));
+        newItems.forEach(newItem => this.items.set(newItem.name, newItem));
     }
     get(name) {
-        return [...this.items.values()].find(item => item.name === name);
+        return this.items.get(name);
     }
     get count() {
         return this.items.size;
@@ -30,7 +22,7 @@ let productCollection = new Collection(products);
 console.log(`There are ${productCollection.count} products`);
 let p = productCollection.get("Hat");
 console.log(`Product: ${p.name}, ${p.price}`);
-// The Collection<T> class has been changed to Set<T> to store its items, which it does
-// by using its generic type parameter for the collection. THE TS compiler uses the type
-// parameter to prevent other data types from being added to the set, and no type guarding
-// is required when retrieving objects from the collection.
+// Generic classes dont have to provide generic type parameters for collections and can
+// specify concrete types instead. A Map is used to store objects using the name property
+// as a key. The name property can be used safely because it is part of the restriction 
+// applied to the type paramter named T.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,16 +1,4 @@
-// Using Generic Collections
-
-// TS provides support for using the JS collectiions with generic type parameters 
-// allowing a generic classs to safely use collections. 
-
-// The Generic Collection Types
-//            Name                     Description
-//         Map<K, V>         Map whose key type is K and whose value type is V. 
-//   ReadonlyMap<K, V>       Map that cannot be modified.
-//         Set<T>            Set whose value type is T.
-//   ReadonlySet<T>          Set that cannot be modified.
-
-// Using a Collection
+// Using A Map.
 
 import { City, Person, Product, Employee } from "./dataTypes";
 
@@ -19,18 +7,19 @@ let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
 type  shapeType = { name: string };
 
 class Collection<T extends shapeType> {
-    private items: Set<T>;
+    private items: Map<string, T>;
 
     constructor(initialItems: T[] = []) {
-        this.items = new Set<T>(initialItems);
+        this.items = new Map<string, T>();
+        this.add(...initialItems);
     }
     
     add(...newItems: T[]): void{
-        newItems.forEach(newItem => this.items.add(newItem));
+        newItems.forEach(newItem => this.items.set(newItem.name, newItem));
 }
 
     get(name: string): T {
-        return [...this.items.values()].find(item => item.name === name);
+        return this.items.get(name);
     }
 
     get count(): number {
@@ -43,7 +32,7 @@ console.log(`There are ${ productCollection.count } products`);
 let p = productCollection.get("Hat");
 console.log(`Product: ${ p.name }, ${ p.price }`);
 
-// The Collection<T> class has been changed to Set<T> to store its items, which it does
-// by using its generic type parameter for the collection. THE TS compiler uses the type
-// parameter to prevent other data types from being added to the set, and no type guarding
-// is required when retrieving objects from the collection.
+// Generic classes dont have to provide generic type parameters for collections and can
+// specify concrete types instead. A Map is used to store objects using the name property
+// as a key. The name property can be used safely because it is part of the restriction 
+// applied to the type paramter named T.
