f9cef9253ff02b5242b71ed2efd4d5670c403f3d
OmarMWarraich <earthling986@gmail.com>
2021-05-28 20:23:50 -0400

    Define a Generic Interface

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,35 +1,10 @@
 "use strict";
-// Adding a Type Parameter
+// Defining Generic Interfaces
 Object.defineProperty(exports, "__esModule", { value: true });
-const dataTypes_1 = require("./dataTypes");
-let people = [new dataTypes_1.Person("Bob Smith", "London"),
-    new dataTypes_1.Person("Dora Peters", "New York")];
-let products = [new dataTypes_1.Product("Running Shoes", 100), new dataTypes_1.Product("Hat", 25)];
-let cities = [new dataTypes_1.City("London", 8136000), new dataTypes_1.City("Paris", 2141000)];
-let employees = [new dataTypes_1.Employee("Bob Smith", "Sales"),
-    new dataTypes_1.Employee("Alice Jones", "Sales")];
-class DataCollection {
-    constructor(initalItems) {
-        this.items = [];
-        this.items.push(...initalItems);
-    }
-    filter(predicate) {
-        return this.items.filter(item => predicate(item));
-    }
-    static reverse(items) {
-        return items.reverse();
-    }
-}
-let mixedData = new DataCollection([...people, ...products]);
-function isProduct(target) {
-    return target instanceof dataTypes_1.Product;
-}
-let filteredProducts = mixedData.filter(isProduct);
-filteredProducts.forEach(p => console.log(`Product: ${p.name}, ${p.price}`));
-let reversedCities = DataCollection.reverse(cities);
-reversedCities.forEach(c => console.log(`City: ${c.name}, ${c.population}`));
-// The reverse method defines a type parameter that specifies the array type it processes.
-// When the method is invoked, it is done so through the DataCollection class and a type 
-// argument is provided after the method name.
-// The type parameters defined by static methods are separate from those defined by the
-// class for use by its instance properties and methods.
+//The Collection<T> interface has a generic type parameter named T, following the same 
+// syntax used for class type parameters. The type parameter is used by the add and get
+// methods, and it has been constrained to ensure that only types that have a name
+// property can be used.
+// An interface with a generic type parameter describes a set of abstract operations but
+// doesnt specify which types they can be performed on, leaving specific types to be
+// selected by derived dinterfaces or implementation classes.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,46 +1,24 @@
-// Adding a Type Parameter
+// Defining Generic Interfaces
+
+// Interfaces can be defined with generic type parameters, allowing functionality to be
+// defined without specifying individual types. Below an interface defined with a generic
+// type parameter.
 
 import { City, Person, Product, Employee } from './dataTypes';
 
-let people = [new Person("Bob Smith", "London"),
-    new Person("Dora Peters", "New York")];
-let products = [new Product("Running Shoes", 100), new Product("Hat", 25)];
-let cities = [new City("London", 8136000), new City("Paris", 2141000)];
-let employees = [new Employee("Bob Smith", "Sales"),
-    new Employee("Alice Jones", "Sales")];
+type shapeType = { name: string };
 
+interface Collection<T extends shapeType> {
 
-
-class DataCollection<T> {
-
-    protected items: T[] = [];
-
-    constructor(initalItems: T[]) {
-        this.items.push(...initalItems);
-    }
-
-    filter<V extends T>(predicate: (target) => target is V): V[] {
-        return this.items.filter(item => predicate(item)) as V[];
-    }
-
-    static reverse<ArrayType>(items: ArrayType[]): ArrayType[] {
-        return items.reverse();
-    }
+    add(...newItems: T[]): void;
+    get(name: string): T;
+    count: number;
 }
 
-let mixedData = new DataCollection<Person | Product>([...people, ...products]);
-function isProduct(target): target is Product {
-    return target instanceof Product;
-}
-let filteredProducts = mixedData.filter<Product>(isProduct);
-filteredProducts.forEach(p => console.log(`Product: ${p.name}, ${p.price}`));
-
-let reversedCities = DataCollection.reverse<City>(cities);
-reversedCities.forEach(c => console.log(`City: ${c.name}, ${c.population}`));
-
-// The reverse method defines a type parameter that specifies the array type it processes.
-// When the method is invoked, it is done so through the DataCollection class and a type 
-// argument is provided after the method name.
-
-// The type parameters defined by static methods are separate from those defined by the
-// class for use by its instance properties and methods.
+//The Collection<T> interface has a generic type parameter named T, following the same 
+// syntax used for class type parameters. The type parameter is used by the add and get
+// methods, and it has been constrained to ensure that only types that have a name
+// property can be used.
+// An interface with a generic type parameter describes a set of abstract operations but
+// doesnt specify which types they can be performed on, leaving specific types to be
+// selected by derived dinterfaces or implementation classes.
