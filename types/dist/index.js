a1c2fd57fdb90accd6ab4b287ba7df6fa03b796a
OmarMWarraich <earthling986@gmail.com>
2021-05-28 22:59:39 -0400

    Use the Indexed Access Operator with a Generic Type

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,26 +1,22 @@
 "use strict";
-// Using the Indexed Access Operator
+// Using the Indexed Access Operator with a Generic Type
 Object.defineProperty(exports, "__esModule", { value: true });
-// The indexed access operator is used to get the type from one or more properties.
 const dataTypes_1 = require("./dataTypes");
 function getValue(item, keyname) {
-    console.log(`Value: ${item[keyname]}`);
+    return item[keyname];
 }
 let p = new dataTypes_1.Product("Running Shoes", 100);
-getValue(p, "name");
-getValue(p, "price");
+console.log(getValue(p, "name"));
+console.log(getValue(p, "price"));
 let e = new dataTypes_1.Employee("Bob Smith", "Sales");
-getValue(e, "name");
-getValue(e, "role");
-// The indexed access operator is expressed using square brackets following a type so 
-// that Product["price"], e.g, is number, since that is the type of the price property
-// defined by the Product class. The indexed access operator works on literal value types,
-// which means it can be used with index type queries.
-// The keyof Product expression returns a literal value type union with the property 
-// names defined by the Product clas, "name" | "price". The indexed access operator 
-// returns the union of the types of those properties, such that Product[keyof Product]
-// is string | number, which is the union of the types of the name and price properties.
-// The types returned by the indexed access operator are known as lookup types.
-// The indexed access operator is most commonly used with generic types, which allows
-// property types to be handled safely even though the specific types that will be used
-// are unknown.
+console.log(getValue(e, "name"));
+console.log(getValue(e, "role"));
+// Theindexed access operator is expressed using a regular type, its keyof shape, and
+// square brackets.
+// The indexed access operator
+// The indexed access operator above, T[K], tells the compiler that the result of the
+// getValue function will have the type of the property whose name is specified by the
+// keyof type argument, leaving the compiler to determine the result types based on the
+// generic type arguments used to invoke the function. For the Product object, that means
+// a name argument will produce a string result, and a price argument will produce a 
+// number result.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,37 +1,28 @@
-// Using the Indexed Access Operator
+// Using the Indexed Access Operator with a Generic Type
 
-// The indexed access operator is used to get the type from one or more properties.
 
 import { City, Person, Product, Employee } from "./dataTypes";
 
-function getValue<T, K extends keyof T>(item: T, keyname: K) {
-    console.log(`Value: ${item[keyname]}`);
+function getValue<T, K extends keyof T>(item: T, keyname: K): T[K] {
+    return item[keyname];
 }
 
-type priceType = Product["price"];
-type allTypes = Product[keyof Product];
-
 let p = new Product("Running Shoes", 100);
-getValue<Product, "name">(p, "name");
-getValue(p, "price");
+console.log(getValue<Product, "name">(p, "name"));
+console.log(getValue(p, "price"));
 
 let e = new Employee("Bob Smith", "Sales");
-getValue(e, "name");
-getValue(e, "role");
+console.log(getValue(e, "name"));
+console.log(getValue(e, "role"));
 
+// Theindexed access operator is expressed using a regular type, its keyof shape, and
+// square brackets.
 
-// The indexed access operator is expressed using square brackets following a type so 
-// that Product["price"], e.g, is number, since that is the type of the price property
-// defined by the Product class. The indexed access operator works on literal value types,
-// which means it can be used with index type queries.
+// The indexed access operator
 
-// The keyof Product expression returns a literal value type union with the property 
-// names defined by the Product clas, "name" | "price". The indexed access operator 
-// returns the union of the types of those properties, such that Product[keyof Product]
-// is string | number, which is the union of the types of the name and price properties.
-
-// The types returned by the indexed access operator are known as lookup types.
-
-// The indexed access operator is most commonly used with generic types, which allows
-// property types to be handled safely even though the specific types that will be used
-// are unknown.
+// The indexed access operator above, T[K], tells the compiler that the result of the
+// getValue function will have the type of the property whose name is specified by the
+// keyof type argument, leaving the compiler to determine the result types based on the
+// generic type arguments used to invoke the function. For the Product object, that means
+// a name argument will produce a string result, and a price argument will produce a 
+// number result.
