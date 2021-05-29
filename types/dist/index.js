2bb22609ff9df54d9dfad1f33f9001337addc511
OmarMWarraich <earthling986@gmail.com>
2021-05-28 22:36:19 -0400

    Constrain a Generic Type Parameter

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,12 +1,23 @@
 "use strict";
-// Using Index Types
+// Constraining a Generic Type Parameter
 Object.defineProperty(exports, "__esModule", { value: true });
-let myVar = "name";
-myVar = "price";
-myVar = "someOtherName";
-// The type annotation for the myVar variable is keyof Product, which will be the union
-// of the property names defined by the Product class. The result is that myVar can be
-// assigned only the 'string values' 'name' and 'price' because these are the names of the
-// only 'two properties' defined by the Product class in the 'dataTypes.ts'.
-// The keyof keyword can be used to constrain generic type parameters so that they can 
-// only be typed to match the properties of another type.
+// the keyof keyword can be used to constrain generic type parameters so that they can 
+// only be types to match the properties of another type.
+const dataTypes_1 = require("./dataTypes");
+function getValue(item, keyname) {
+    console.log(`Value: ${item[keyname]}`);
+}
+let p = new dataTypes_1.Product("Running Shoes", 100);
+getValue(p, "name");
+getValue(p, "price");
+let e = new dataTypes_1.Employee("Bob Smith", "Sales");
+getValue(e, "name");
+getValue(e, "role");
+// The example defines a function named getValue, whose key parameter K is constrained
+// using typeof T, which means that K can be the name of only one of the properties
+// defined by T, regardless of the type used for T when the function is invoked. When
+// the getValue function is used with a Product object, the keyname parameter can be only
+// name or price. And when the getVAlue function is used with an Employee object, the
+// keyname parameter can be only name or role. In both cases, the keyname parameter can 
+// be used to safely get or set the value of the corresponding property from the Product
+// or Employee object.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,29 +1,28 @@
-// Using Index Types
+// Constraining a Generic Type Parameter
 
-// The Collection<T> class restricts the types it can accept using a shape type, which
-// ensures that all the objects it deals with have a name property that can be used as 
-// the key to store and retrieve objects in the Map. 
-
-// TS provides a set of related features that allow any property defined by an object to
-// be used as a key while preserving type safety. These features can be difficult to
-// understand.
-
- // Using the Index Type Query
-
- // The keyof keyword known as the index type query operator returns a union of the property
- // names of a type, using the literal value type feature.
-
+// the keyof keyword can be used to constrain generic type parameters so that they can 
+// only be types to match the properties of another type.
 
 import { City, Person, Product, Employee } from "./dataTypes";
 
-let myVar: keyof Product = "name";
-myVar = "price";
-myVar = "someOtherName";
+function getValue<T, K extends keyof T>(item: T, keyname: K) {
+    console.log(`Value: ${item[keyname]}`);
+}
 
-// The type annotation for the myVar variable is keyof Product, which will be the union
-// of the property names defined by the Product class. The result is that myVar can be
-// assigned only the 'string values' 'name' and 'price' because these are the names of the
-// only 'two properties' defined by the Product class in the 'dataTypes.ts'.
+let p = new Product("Running Shoes", 100);
+getValue(p, "name");
+getValue(p, "price");
 
-// The keyof keyword can be used to constrain generic type parameters so that they can 
-// only be typed to match the properties of another type.
+let e = new Employee("Bob Smith", "Sales");
+getValue(e, "name");
+getValue(e, "role");
+
+
+// The example defines a function named getValue, whose key parameter K is constrained
+// using typeof T, which means that K can be the name of only one of the properties
+// defined by T, regardless of the type used for T when the function is invoked. When
+// the getValue function is used with a Product object, the keyname parameter can be only
+// name or price. And when the getVAlue function is used with an Employee object, the
+// keyname parameter can be only name or role. In both cases, the keyname parameter can 
+// be used to safely get or set the value of the corresponding property from the Product
+// or Employee object.
