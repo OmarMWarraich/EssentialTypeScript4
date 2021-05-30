0baf7ff39dcee3bf57411b9f4ff7d03487003cce
OmarMWarraich <earthling986@gmail.com>
2021-05-30 11:28:55 -0400

    Add a bundler

---
+++ webApp/dist/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.log("Web App");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJBcHAvLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBYTtBQUNiIiwiZmlsZSI6ImJ1bmRsZS5qcyIsInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xuY29uc29sZS5sb2coXCJXZWIgQXBwXCIpO1xuIl0sInNvdXJjZVJvb3QiOiIifQ==
