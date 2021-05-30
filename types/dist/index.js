aa4a855ff10e0a44472b7fef33346f53c38765f3
OmarMWarraich <earthling986@gmail.com>
2021-05-29 20:44:11 -0400

    Create Types with a Type Mapping

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,11 +1,17 @@
 "use strict";
-// Combining Transformations in a Single Mapping
+// Create Types with a Type Mapping
 Object.defineProperty(exports, "__esModule", { value: true });
-let p1 = { name: "Kayak" };
-let p2 = { name: "Lifejacket", price: 48.95 };
-console.log(`Custom mapped type: ${p1.name}`);
-console.log(`Built-in mapped type (Pick): ${p2.name}, ${p2.price}`);
-// For custom type mappings, the ? and readonly keyword can be applied in the same 
-// transformation, which can be constrained to allow properties to be selected by name.
-// Mappings can also be chained together, as shown by the combination of the Pick, Partial
-// and Readonly mappings.
+let p1 = { name: "Bob", city: "London" };
+let p2 = { name: "Alice", city: "Paris" };
+console.log(`Custom mapped type: ${p1.name}, ${p1.city}`);
+console.log(`Built-in mapped type (Pick): ${p2.name}, ${p2.city}`);
+// The first generic type parameter is restricted using keyof any, which means that a
+// literal value type union can be specified and that it can contain the property names
+// required for the new type. The second generic type parameter is used to specify the
+// type for the properties that are created and is used like this.
+// let p1: CustomMapped<"name" | "city", string> = { name: "Bob", city: "London" };
+// The mapping produces a type with two string properties: name and city. TS provides
+// the builtin Record mapping, which performs the same task.
+// let p2: Record<"name"| "city", string> = { name: "Alice", city: "Paris"};
+//Mappings are more flexible than they might appear and that literal value types restricted
+// by keyof any can accept any combination of property names.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,26 +1,34 @@
-// Combining Transformations in a Single Mapping
+// Create Types with a Type Mapping
 
-// Second last commit namely 'Built-in Mappings' showed how mappings can be combined to
-// create a chain of transformations, but mappings can apply multiple changes to 
-// properties.
+// The final feature provided by type mappings is the ability to create new types, rather
+// than transform a specific one. Below shows the basic use of this feature, which creates
+// a type that contains name and city properties.
 
-// Combining Transformations in a Single Mapping
+// Creating a Type.
 
 import { City, Person, Product,Employee } from "./dataTypes";
 
-type CustomMapped<T, K extends keyof T> = {
-    readonly[P in K]?: T[P]
+type CustomMapped<K extends keyof any, T> = {
+    [P in K]: T
 };
 
-type BuiltInMapped<T, K extends keyof T> = Readonly<Partial<Pick<T, K>>>;
 
-let p1: CustomMapped<Product, "name"> = { name: "Kayak" };
-let p2: BuiltInMapped<Product, "name" | "price" > = { name: "Lifejacket", price: 48.95};
 
-console.log(`Custom mapped type: ${p1.name}`);
-console.log(`Built-in mapped type (Pick): ${p2.name}, ${p2.price}`);
+let p1: CustomMapped<"name" | "city", string> = { name: "Bob", city: "London" };
+let p2: Record<"name" | "city", string > = { name: "Alice", city: "Paris" };
 
-// For custom type mappings, the ? and readonly keyword can be applied in the same 
-// transformation, which can be constrained to allow properties to be selected by name.
-// Mappings can also be chained together, as shown by the combination of the Pick, Partial
-// and Readonly mappings.
+console.log(`Custom mapped type: ${p1.name}, ${p1.city}`);
+console.log(`Built-in mapped type (Pick): ${p2.name}, ${p2.city}`);
+
+// The first generic type parameter is restricted using keyof any, which means that a
+// literal value type union can be specified and that it can contain the property names
+// required for the new type. The second generic type parameter is used to specify the
+// type for the properties that are created and is used like this.
+// let p1: CustomMapped<"name" | "city", string> = { name: "Bob", city: "London" };
+
+// The mapping produces a type with two string properties: name and city. TS provides
+// the builtin Record mapping, which performs the same task.
+// let p2: Record<"name"| "city", string> = { name: "Alice", city: "Paris"};
+
+//Mappings are more flexible than they might appear and that literal value types restricted
+// by keyof any can accept any combination of property names.
