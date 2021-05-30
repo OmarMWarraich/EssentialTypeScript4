ee527b39114a5328821135785dc2383ed3be887a
OmarMWarraich <earthling986@gmail.com>
2021-05-29 20:30:38 -0400

    Combine Mappings in A Single Transformation

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,14 +1,11 @@
 "use strict";
-// Mapping Specific Properties
+// Combining Transformations in a Single Mapping
 Object.defineProperty(exports, "__esModule", { value: true });
 let p1 = { name: "Kayak" };
-let p2 = { name: "Kayak" };
-let p3 = { name: "Kayak" };
+let p2 = { name: "Lifejacket", price: 48.95 };
 console.log(`Custom mapped type: ${p1.name}`);
-console.log(`Built-in mapped type (Pick): ${p2.name}`);
-console.log(`Built-in mapped type (Omit): ${p3.name}`);
-// The SelectProperties mapping defines an additional generic type parameter named K that
-// is restricted using keyof so that only types that correspond to properties defined by
-// the type parameter can be specified. The new type parameter is used in the mapping's 
-// name selector, with the result that individual properties cn be selected for inclusion
-// in the mapped type. all 3 results "Kayak"
+console.log(`Built-in mapped type (Pick): ${p2.name}, ${p2.price}`);
+// For custom type mappings, the ? and readonly keyword can be applied in the same 
+// transformation, which can be constrained to allow properties to be selected by name.
+// Mappings can also be chained together, as shown by the combination of the Pick, Partial
+// and Readonly mappings.
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,25 +1,26 @@
-// Mapping Specific Properties
+// Combining Transformations in a Single Mapping
 
-// The index type query for a mapped type can be expressed as a generic type parameter,
-// which can then be used to select specific properties to map by name.
+// Second last commit namely 'Built-in Mappings' showed how mappings can be combined to
+// create a chain of transformations, but mappings can apply multiple changes to 
+// properties.
 
-// Map Specific Properties.
+// Combining Transformations in a Single Mapping
 
 import { City, Person, Product,Employee } from "./dataTypes";
 
-type SelectProperties<T, K extends keyof T> = {
-    [P in K]: T[P]
+type CustomMapped<T, K extends keyof T> = {
+    readonly[P in K]?: T[P]
 };
 
-let p1: SelectProperties<Product, "name"> = { name: "Kayak" };
-let p2: Pick<Product, "name"> = { name: "Kayak"};
-let p3: Omit<Product, "price"> = { name: "Kayak" };
-console.log(`Custom mapped type: ${p1.name}`);
-console.log(`Built-in mapped type (Pick): ${p2.name}`);
-console.log(`Built-in mapped type (Omit): ${p3.name}`);
+type BuiltInMapped<T, K extends keyof T> = Readonly<Partial<Pick<T, K>>>;
 
-// The SelectProperties mapping defines an additional generic type parameter named K that
-// is restricted using keyof so that only types that correspond to properties defined by
-// the type parameter can be specified. The new type parameter is used in the mapping's 
-// name selector, with the result that individual properties cn be selected for inclusion
-// in the mapped type. all 3 results "Kayak"
+let p1: CustomMapped<Product, "name"> = { name: "Kayak" };
+let p2: BuiltInMapped<Product, "name" | "price" > = { name: "Lifejacket", price: 48.95};
+
+console.log(`Custom mapped type: ${p1.name}`);
+console.log(`Built-in mapped type (Pick): ${p2.name}, ${p2.price}`);
+
+// For custom type mappings, the ? and readonly keyword can be applied in the same 
+// transformation, which can be constrained to allow properties to be selected by name.
+// Mappings can also be chained together, as shown by the combination of the Pick, Partial
+// and Readonly mappings.
