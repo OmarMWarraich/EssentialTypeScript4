543b38b6644fd8d806f9d379b0e30576dee53275
OmarMWarraich <earthling986@gmail.com>
2021-05-29 21:08:41 -0400

    Nest Conditional Types

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,30 +1,13 @@
 "use strict";
-// Use Conditional Types
+// Nesting Conditional Types
 Object.defineProperty(exports, "__esModule", { value: true });
-let firstVal = "String Value";
-let secondVal = 100;
-let mismatchCheck = "String Value";
-// Conditional Types have a generic type parameter and a ternary expression that 
-// selects a result type.
-// A conditional type is a placeholder for one of its result types, which isnt chosen
-// until the generic type parameter is used, which allows the expression to be 
-// evaluated using one of the result types selected.
-// Above, the resultType<T> conditional type is a placeholder for the string and number
-// types, meaning that the argument for the generic type T will determine whether the 
-// conditional type resolves to string or number. The generic type parameter T is 
-// restricted so that it can only accept boolean values, and the expression will evaluate
-// as true if the argument provided for T is the literal value type true. The result is 
-// that resultType<T> resolves to string when T is true.
-// ...
-// let firstVal: resultType<true> = "String Value";
-// let stringTypeCheck: string = firstVal;
-// ...
-// The compiler resolves the conditional type and knows that the type annotation for 
-// firstVal resolves to string, allowing a string literal value to be assigned to firstVal.
-// When the generic type argument is false, the conditional type resolves to number.
-// ...
-// let secondVal: resultType<false> = 100;
-// let numberTypeCheck: number = secondVal;
-// The compiler enforces type safety with conditional types. Final statement: conditional
-// type resolves to number but is assigned a string value producing compiler error;
-// TS2322: Type 'String Value' is not assignable to type 'number'.
+// More complex combinations of types can be described by nesting conditional types. A 
+// conditional type's result can be another conditional type, and the compiler will
+// follow the chain of expressions until it reaches a result that isn't condtional.
+// Nesting Conditional Types
+const dataTypes_1 = require("./dataTypes");
+let firstVal = new dataTypes_1.City("London", 8136000);
+let secondVal = new dataTypes_1.Person("Bob", "London");
+let thirdVal = new dataTypes_1.Product("Kayak", 275);
+// the type nestedType<T> is a nested conditional type to select between three types,
+// based on the value of the generic type paramter. 
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,46 +1,23 @@
-// Use Conditional Types
+// Nesting Conditional Types
 
-// Conditional types are expressions containing generic type parameters that are 
-// evaluated to select new types. Below a basic conditional type.
+// More complex combinations of types can be described by nesting conditional types. A 
+// conditional type's result can be another conditional type, and the compiler will
+// follow the chain of expressions until it reaches a result that isn't condtional.
 
-// Using a Conditional Type.
+// Nesting Conditional Types
 
-import { City, Person, Product,Employee } from "./dataTypes";
+import { City, Person, Product, Employee } from './dataTypes';
 
 type resultType<T extends boolean> = T extends true ? string : number;
 
-let firstVal: resultType<true> = "String Value";
-let secondVal: resultType<false> = 100;
+type references = "London" | "Bob" | "Kayak";
 
-let mismatchCheck: resultType<false> = "String Value";
+type nestedType<T extends references> 
+    = T extends "London" ? City : T extends "Bob" ? Person : Product;
 
-// Conditional Types have a generic type parameter and a ternary expression that 
-// selects a result type.
+    let firstVal: nestedType<"London"> = new City("London", 8136000);
+    let secondVal: nestedType<"Bob"> = new Person("Bob", "London");
+    let thirdVal: nestedType<"Kayak"> = new Product("Kayak", 275);
 
-// A conditional type is a placeholder for one of its result types, which isnt chosen
-// until the generic type parameter is used, which allows the expression to be 
-// evaluated using one of the result types selected.
-
-// Above, the resultType<T> conditional type is a placeholder for the string and number
-// types, meaning that the argument for the generic type T will determine whether the 
-// conditional type resolves to string or number. The generic type parameter T is 
-// restricted so that it can only accept boolean values, and the expression will evaluate
-// as true if the argument provided for T is the literal value type true. The result is 
-// that resultType<T> resolves to string when T is true.
-// ...
-// let firstVal: resultType<true> = "String Value";
-// let stringTypeCheck: string = firstVal;
-// ...
-// The compiler resolves the conditional type and knows that the type annotation for 
-// firstVal resolves to string, allowing a string literal value to be assigned to firstVal.
-// When the generic type argument is false, the conditional type resolves to number.
-
-// ...
-// let secondVal: resultType<false> = 100;
-// let numberTypeCheck: number = secondVal;
-
-// The compiler enforces type safety with conditional types. Final statement: conditional
-// type resolves to number but is assigned a string value producing compiler error;
-// TS2322: Type 'String Value' is not assignable to type 'number'.
-
-
+// the type nestedType<T> is a nested conditional type to select between three types,
+// based on the value of the generic type paramter. 
