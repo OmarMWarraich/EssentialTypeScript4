7c7f14ac59c6f5cca16a701d23b962c0219db9c1
OmarMWarraich <earthling986@gmail.com>
2021-05-29 21:26:11 -0400

    Use Conditional Types in Generic Classes

--- types/dist/index.js
+++ types/dist/index.js
@@ -1,13 +1,45 @@
 "use strict";
-// Nesting Conditional Types
+// Using Conditional Types in Generic Classes
 Object.defineProperty(exports, "__esModule", { value: true });
-// More complex combinations of types can be described by nesting conditional types. A 
-// conditional type's result can be another conditional type, and the compiler will
-// follow the chain of expressions until it reaches a result that isn't condtional.
-// Nesting Conditional Types
+// Conditional types can be used to express the relationship between a method or function's
+// paramter types and the results it produces. This is a more concise alternative to 
+// the function type overloading.
+// Use Conditional Types in Generic Classes
 const dataTypes_1 = require("./dataTypes");
-let firstVal = new dataTypes_1.City("London", 8136000);
-let secondVal = new dataTypes_1.Person("Bob", "London");
-let thirdVal = new dataTypes_1.Product("Kayak", 275);
-// the type nestedType<T> is a nested conditional type to select between three types,
-// based on the value of the generic type paramter. 
+class Collection {
+    constructor(...initialItems) {
+        this.items = initialItems || [];
+    }
+    total(propName, format) {
+        let totalValue = this.items.reduce((t, item) => t += Number(item[propName]), 0);
+        return format ? `$${totalValue.toFixed()}` : totalValue;
+    }
+}
+let data = new Collection(new dataTypes_1.Product("Kayak", 275), new dataTypes_1.Product("Life Jacket", 48.95));
+let firstVal = data.total("price", true);
+console.log(`Formatted value: ${firstVal}`);
+let secondVal = data.total("price", false);
+console.log(`Unformatted value: ${secondVal}`);
+// The Collection<T> class uses an array to store objects whose type is specified by the
+// generic type parameter named T. The total method defiens two generic type parameters: P,
+// which specifies a property to use to create a total, and U, which specifies whether the 
+// result should be formatted. The result of the total method is a conditional type, which
+// is resolved using the value provided for the type parameter U.
+//...
+//total<P extends keyof T, U extends boolean>(propName: P, format: U): resultType<U>{}
+//...
+//The use of the conditional type means that the result of the total method is determined
+// by the argument provided for the type paramter U. Since the compiler can infer U from
+// the value provided for the argument format, the method can be invoked like this.
+//...
+//let firstVal: string = data.total("price", true);
+//...
+// When arg 4 format param is true, the conditional type resolves to set the result type
+// of the total method to string. This matches the data type produced by method implementation.
+//...
+// return format ? `$${totalValue.toFixed()}` : totalValue as any;
+//...
+// When arg 4 format param is false,the conditional type resolves to set the result type
+// of the total method to number.allowing the method to rtrn da unformatted value number.
+//...
+// return format ? `$${totalValue.toFixed()}` : totalValue as any;
--- types/src/index.ts
+++ types/src/index.ts
@@ -1,23 +1,59 @@
-// Nesting Conditional Types
+// Using Conditional Types in Generic Classes
 
-// More complex combinations of types can be described by nesting conditional types. A 
-// conditional type's result can be another conditional type, and the compiler will
-// follow the chain of expressions until it reaches a result that isn't condtional.
+// Conditional types can be used to express the relationship between a method or function's
+// paramter types and the results it produces. This is a more concise alternative to 
+// the function type overloading.
 
-// Nesting Conditional Types
+// Use Conditional Types in Generic Classes
 
 import { City, Person, Product, Employee } from './dataTypes';
 
 type resultType<T extends boolean> = T extends true ? string : number;
 
-type references = "London" | "Bob" | "Kayak";
+class Collection<T> {
+    private items: T[];
 
-type nestedType<T extends references> 
-    = T extends "London" ? City : T extends "Bob" ? Person : Product;
+    constructor(...initialItems: T[]) {
+        this.items = initialItems || [];
+    }
 
-    let firstVal: nestedType<"London"> = new City("London", 8136000);
-    let secondVal: nestedType<"Bob"> = new Person("Bob", "London");
-    let thirdVal: nestedType<"Kayak"> = new Product("Kayak", 275);
+    total<P extends keyof T, U extends boolean>(propName: P, format: U)
+            : resultType<U> {
+                let totalValue = this.items.reduce((t, item) => 
+                    t += Number(item[propName]), 0);
+                return format ? `$${totalValue.toFixed()}` : totalValue as any;
+            }
+}
 
-// the type nestedType<T> is a nested conditional type to select between three types,
-// based on the value of the generic type paramter. 
+let data = new Collection<Product>(new Product("Kayak", 275), new Product("Life Jacket", 48.95));
+
+let firstVal: string = data.total("price", true);
+console.log(`Formatted value: ${firstVal}`);
+let secondVal: number = data.total("price", false);
+console.log(`Unformatted value: ${secondVal}`);
+
+// The Collection<T> class uses an array to store objects whose type is specified by the
+// generic type parameter named T. The total method defiens two generic type parameters: P,
+// which specifies a property to use to create a total, and U, which specifies whether the 
+// result should be formatted. The result of the total method is a conditional type, which
+// is resolved using the value provided for the type parameter U.
+//...
+//total<P extends keyof T, U extends boolean>(propName: P, format: U): resultType<U>{}
+//...
+//The use of the conditional type means that the result of the total method is determined
+// by the argument provided for the type paramter U. Since the compiler can infer U from
+// the value provided for the argument format, the method can be invoked like this.
+//...
+//let firstVal: string = data.total("price", true);
+//...
+// When arg 4 format param is true, the conditional type resolves to set the result type
+// of the total method to string. This matches the data type produced by method implementation.
+//...
+// return format ? `$${totalValue.toFixed()}` : totalValue as any;
+//...
+// When arg 4 format param is false,the conditional type resolves to set the result type
+// of the total method to number.allowing the method to rtrn da unformatted value number.
+//...
+// return format ? `$${totalValue.toFixed()}` : totalValue as any;
+
+
