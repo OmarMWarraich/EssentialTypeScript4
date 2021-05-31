05783a3b634d14d0281cdce7c81a7465d152112c
OmarMWarraich <earthling986@gmail.com>
2021-05-31 09:55:17 -0400

    Update src/index.ts

--- webApp/src/index.ts
+++ webApp/src/index.ts
@@ -1,2 +1,22 @@
-console.log("Web App");
-console.log("This is a new statement");
+import { LocalDataSource } from "./data/localDataSource";
+
+async function displayData(): Promise<string> {
+    let ds = new LocalDataSource();
+    let allProducts = await ds.getProducts("name");
+    let categories = await ds.getCategories();
+    let chessProducts = await ds.getProducts("name", "Chess");
+
+    let result = "";
+
+    allProducts.forEach(p => result += `Product: ${p.name}, ${p.category}\n`);
+    categories.forEach(c => result += (`Category: ${c}\n`));
+    chessProducts.forEach(p => ds.order.addProduct(p, 1));
+    result += `Order total: $${ds.order.total.toFixed(2)}`;
+    return result;
+}
+
+displayData().then(res => console.log(res));
+
+// When the changes to the index.ts file are saved, the code will be compiled and the chain
+// of import statements is resolved to include all the JS required by the application in the
+// webpack bundle. 
