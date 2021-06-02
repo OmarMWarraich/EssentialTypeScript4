bbfc2c91c99304ff6a2816a164362c861ac2428d
OmarMWarraich <earthling986@gmail.com>
2021-06-02 04:25:38 -0400

    Display HTML Content

--- webApp/src/domDisplay.ts
+++ webApp/src/domDisplay.ts
@@ -10,7 +10,7 @@ export class DomDisplay {
     getContent(): HTMLElement {
         let elem = document.createElement("h3");
         elem.innerText = this.getElementText();
-        elem.classList.add("bg-primary", "text-center", "text-white", "p-2  ");
+        elem.classList.add("bg-primary", "text-center", "text-white", "p-2");
         return elem;
     }
 
